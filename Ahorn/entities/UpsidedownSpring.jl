b009fc59379abacafdef8fe6b21defa8d9852904
Lollyde <github@lollyde.online>
2021-02-23 00:11:23 +0100

    fix ahorn module whitespace

--- Ahorn/entities/UpsidedownSpring.jl
+++ Ahorn/entities/UpsidedownSpring.jl
@@ -4,21 +4,20 @@ using ..Ahorn, Maple
 @mapdef Entity "SJ2021/UpsidedownSpring" UpsidedownSpring(x::Integer, y::Integer, strength::Number=1.0, xAxisFriction::Number=0.5)
 
 const placements = Ahorn.PlacementDict(
-   "Upside down Spring (AntiGravJelly only) (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
+    "Upside down Spring (AntiGravJelly only) (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
     UpsidedownSpring,
     "rectangle"
-   )
+    )
 )
 
 
 function Ahorn.selection(entity::UpsidedownSpring)
     x, y = Ahorn.position(entity)
- 
     return Ahorn.Rectangle(x - 6, y + 7, 12, 5)
 end
 
 function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::UpsidedownSpring, room::Maple.Room)
-        Ahorn.drawSprite(ctx, "objects/spring/00.png", 12, 6, rot = pi)
+    Ahorn.drawSprite(ctx, "objects/spring/00.png", 12, 6, rot = pi)
 end
 
 end
