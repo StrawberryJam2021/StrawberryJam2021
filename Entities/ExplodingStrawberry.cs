4b59e95576eee8c8d26c2c2b9a69606419da9cb9
bbb651 <bar.ye651@gmail.com>
2021-02-27 00:14:56 +0200

    Basic strawberry entity

---
+++ Entities/ExplodingStrawberry.cs
@@ -0,0 +1,10 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.StrawberryJam2021.Entities {
+    [CustomEntity("SJ2021/ExplodingStrawberry")]
+    [RegisterStrawberry(true, false)]
+    public class ExplodingStrawberry : Strawberry {
+        public ExplodingStrawberry(EntityData data, Vector2 offset, EntityID gid) : base(data, offset, gid) { }
+    }
+}
