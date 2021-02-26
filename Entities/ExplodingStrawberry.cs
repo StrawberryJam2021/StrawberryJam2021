90d8a0be0c277dd8521a7c1e96ffda1895dd3ed2
bbb651 <bar.ye651@gmail.com>
2021-02-27 00:51:36 +0200

    Strawberry exploding functionality

--- Entities/ExplodingStrawberry.cs
+++ Entities/ExplodingStrawberry.cs
@@ -1,10 +1,30 @@
 ﻿using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
+using Monocle;
 
 namespace Celeste.Mod.StrawberryJam2021.Entities {
     [CustomEntity("SJ2021/ExplodingStrawberry")]
     [RegisterStrawberry(true, false)]
+    [Tracked]
     public class ExplodingStrawberry : Strawberry {
         public ExplodingStrawberry(EntityData data, Vector2 offset, EntityID gid) : base(data, offset, gid) { }
+
+        public static void Load() {
+            On.Celeste.Puffer.Explode += OnPufferExplode;
+        }
+
+        public static void Unload() {
+            On.Celeste.Puffer.Explode -= OnPufferExplode;
+        }
+
+        private static void OnPufferExplode(On.Celeste.Puffer.orig_Explode orig, Puffer self) {
+            Engine.Scene.Tracker.GetEntities<ExplodingStrawberry>().ForEach(entity => {
+                ExplodingStrawberry strawberry = entity as ExplodingStrawberry;
+                if (strawberry.Follower.Leader == null) {
+                    Engine.Scene.Remove(strawberry);
+                }
+            });
+            orig(self);
+        }
     }
 }
