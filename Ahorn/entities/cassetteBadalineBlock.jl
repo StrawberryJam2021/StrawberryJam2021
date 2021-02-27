5681f2456f352dad0b3f5a491aef87976d0e072a
Moses Miller <Majora320@gmail.com>
2021-02-26 20:17:40 -0800

    Add cassette-based badaline block entity

---
+++ Ahorn/entities/cassetteBadalineBlock.jl
@@ -0,0 +1,68 @@
+module SJ2021CassetteBadalineBlock
+
+using ..Ahorn, Maple
+
+@pardef CassetteBadalineBlock(x1::Integer, y1::Integer, x2::Integer=x1 + 16, y2::Integer=y1, 
+        width::Integer=Maple.defaultBlockWidth, height::Integer=Maple.defaultBlockHeight, 
+        tiletype::String="g", moveForwardBeat::Integer=0, moveBackBeat::Integer=8, 
+        preDelay::Integer=0, transitionDuration::Integer=4, oneWay::Bool=false, teleportBack::Bool=false) = 
+    Entity("SJ2021/CassetteBadalineBlock", x = x1, y = y1, nodes = Tuple{Int, Int}[(x2, y2)],
+        width = width, height = height, tiletype = tiletype,
+        moveForwardBeat = moveForwardBeat, moveBackBeat = moveBackBeat, preDelay = preDelay, 
+        transitionDuration = transitionDuration, oneWay = oneWay, teleportBack = teleportBack)
+        
+Ahorn.editingOrder(entity::CassetteBadalineBlock) = String["x", "y", "width", "height", 
+    "moveForwardBeat", "moveBackBeat", "preDelay", "transitionDuration", "oneWay", "teleportBack", "tiletype"]
+
+const placements = Ahorn.PlacementDict(
+    "Cassette-based Moving Block (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
+        CassetteBadalineBlock,
+        "rectangle",
+        Dict{String, Any}(),
+        function(entity)
+            entity.data["nodes"] = [(Int(entity.data["x"]) + Int(entity.data["width"]) + 8, Int(entity.data["y"]))]
+        end
+    )
+)
+
+Ahorn.editingOptions(entity::CassetteBadalineBlock) = Dict{String, Any}(
+    "tiletype" => Ahorn.tiletypeEditingOptions(),
+)
+
+Ahorn.nodeLimits(entity::CassetteBadalineBlock) = 1, 1
+Ahorn.minimumSize(entity::CassetteBadalineBlock) = 8, 8
+Ahorn.resizable(entity::CassetteBadalineBlock) = true, true
+
+function Ahorn.selection(entity::CassetteBadalineBlock)
+    if entity.name == "SJ2021/CassetteBadalineBlock"
+        x, y = Ahorn.position(entity)
+        nx, ny = Int.(entity.data["nodes"][1])
+
+        width = Int(get(entity.data, "width", 8))
+        height = Int(get(entity.data, "height", 8))
+
+        return [Ahorn.Rectangle(x, y, width, height), Ahorn.Rectangle(nx, ny, width, height)]
+    end
+end
+
+function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::CassetteBadalineBlock, room::Maple.Room)
+    Ahorn.drawTileEntity(ctx, room, entity, material = get(entity.data, "tiletype", "g")[1], blendIn = false)
+end
+
+function Ahorn.renderSelectedAbs(ctx::Ahorn.Cairo.CairoContext, entity::CassetteBadalineBlock, room::Maple.Room)
+    x, y = Ahorn.position(entity)
+    nodes = get(entity.data, "nodes", ())
+
+    width = Int(get(entity.data, "width", 8))
+    height = Int(get(entity.data, "height", 8))
+    
+    if !isempty(nodes)
+        nx, ny = Int.(nodes[1])
+        cox, coy = floor(Int, width / 2), floor(Int, height / 2)
+
+        fakeTiles = Ahorn.createFakeTiles(room, nx, ny, width, height, get(entity.data, "tiletype", "g")[1], blendIn = false)
+        Ahorn.drawFakeTiles(ctx, room, fakeTiles, room.objTiles, true, nx, ny, clipEdges = true)
+        Ahorn.drawArrow(ctx, x + cox, y + coy, nx + cox, ny + coy, Ahorn.colors.selection_selected_fc, headLength = 6)
+    end
+end
+end
---
+++ Entities/CassetteBadalineBlock.cs
@@ -0,0 +1,204 @@
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Celeste.Mod.StrawberryJam2021.Entities {
+    /* Large parts of this code are copied from Brokemia's non-badaline-dependant moving block helper.
+       Thank you to Brokemia for letting me use his code in this project! */
+
+    [CustomEntity("SJ2021/CassetteBadalineBlock")]
+    public class CassetteBadalineBlock : Solid {
+        private int nodeIndex;
+        private Vector2[] nodes;
+
+        private int moveForwardBeat;
+        private int moveBackBeat;
+        private int preDelay;
+        private int transitionDuration;
+        private bool oneWay;
+        private bool teleportBack;
+
+        private int lastBeat = -1;
+
+        public CassetteBadalineBlock(Vector2[] nodes, float width, float height, char tiletype,
+            int moveForwardBeat, int moveBackBeat, int preDelay, int transitionDuration, bool oneWay, bool teleportBack)
+            : base(nodes[0], width, height, false) {
+            this.nodes = nodes;
+            int newSeed = Calc.Random.Next();
+            Calc.PushRandom(newSeed);
+            TileGrid sprite = GFX.FGAutotiler.GenerateBox(tiletype, (int) Width / 8, (int) Height / 8).TileGrid;
+            Add(sprite);
+            Calc.PopRandom();
+            Calc.PushRandom(newSeed);
+            Calc.PopRandom();
+            Add(new TileInterceptor(sprite, false));
+            Add(new LightOcclude());
+
+            this.moveForwardBeat = moveForwardBeat;
+            this.moveBackBeat = moveBackBeat;
+            this.preDelay = preDelay;
+            this.transitionDuration = transitionDuration;
+            this.oneWay = oneWay;
+            this.teleportBack = teleportBack;
+        }
+
+        public CassetteBadalineBlock(EntityData data, Vector2 offset)
+            : this(data.NodesWithPosition(offset), data.Width, data.Height, data.Char("tiletype", 'g'),
+                data.Int("moveForwardBeat", 0), data.Int("moveBackBeat", 8), data.Int("preDelay", 0),
+                data.Int("transitionDuration", 4), data.Bool("oneWay", false), data.Bool("teleportBack", false)) {
+        }
+
+        public override void Added(Scene scene) {
+            base.Added(scene);
+
+            Level level = scene as Level;
+
+            // This flag tells CassetteBlockManager to do its thing
+            if (!level.HasCassetteBlocks) {
+                level.HasCassetteBlocks = true;
+            }
+
+            // Add a cassette manager if there isn't one already
+            // just using scene.Tracker.GetEntity<CassetteBlockManager>() == null doesn't work here
+            // because EntityList.Add only adds in batches when we explicitly tell it to update
+            // so, reflection saves the day again :)
+            var toAddField = scene.Entities.GetType().GetField("toAdd", BindingFlags.NonPublic | BindingFlags.Instance);
+            List<Entity> toAdd = toAddField.GetValue(scene.Entities) as List<Entity>;
+            if (scene.Tracker.GetEntity<CassetteBlockManager>() == null &&
+                !toAdd.OfType<CassetteBlockManager>().Any()) {
+                level.Add(new CassetteBlockManager());
+            }
+        }
+
+        public override void Update() {
+            base.Update();
+            var manager = Scene.Tracker.GetEntity<CassetteBlockManager>();
+            
+            if (manager == null)
+                return;
+            
+            // Convert 1-indexing to 0-indexing so we can use modulo on this
+            int beat = manager.GetSixteenthNote() - 1;
+            if (beat != lastBeat && beat >= preDelay) {
+                if (beat % 16 == moveBackBeat && teleportBack) {
+                    Teleport();
+                } else if (beat % 16 == moveForwardBeat || beat % 16 == moveBackBeat) {
+                    Move();
+                }
+
+                if (oneWay) {
+                    moveForwardBeat = -1;
+                    moveBackBeat = -1;
+                }
+            }
+
+            lastBeat = beat;
+        }
+
+        private void Move() {
+            // Convert from beats to seconds
+            Level level = Engine.Scene as Level;
+            float actualTransitionDuration = (float) (transitionDuration * (1.0 / 6.0) * level.CassetteBlockTempo);
+
+            nodeIndex++;
+            nodeIndex %= nodes.Length;
+            Vector2 from = Position;
+            Vector2 to = nodes[nodeIndex];
+            Tween tween = Tween.Create(Tween.TweenMode.Oneshot, Ease.CubeIn, actualTransitionDuration, true);
+            tween.OnUpdate = delegate(Tween t) {
+                MoveTo(Vector2.Lerp(from, to, t.Eased));
+            };
+            tween.OnComplete = delegate {
+                if (CollideCheck<SolidTiles>(Position + (to - from).SafeNormalize() * 2f)) {
+                    Audio.Play("event:/game/06_reflection/fallblock_boss_impact", Center);
+                    ImpactParticles(to - from);
+                } else {
+                    StopParticles(to - from);
+                }
+            };
+            Add(tween);
+        }
+
+        private void Teleport() {
+            nodeIndex++;
+            nodeIndex %= nodes.Length;
+            Vector2 to = nodes[nodeIndex];
+            Position = to;
+        }
+
+        private void StopParticles(Vector2 moved) {
+            Level level = SceneAs<Level>();
+            float direction = moved.Angle();
+            if (moved.X > 0f) {
+                Vector2 value = new Vector2(Right - 1f, Top);
+                for (int i = 0; i < Height; i += 4) {
+                    level.Particles.Emit(FinalBossMovingBlock.P_Stop, value + Vector2.UnitY * (2 + i + Calc.Random.Range(-1, 1)), direction);
+                }
+            } else if (moved.X < 0f) {
+                Vector2 value2 = new Vector2(Left, Top);
+                for (int j = 0; j < Height; j += 4) {
+                    level.Particles.Emit(FinalBossMovingBlock.P_Stop, value2 + Vector2.UnitY * (2 + j + Calc.Random.Range(-1, 1)), direction);
+                }
+            }
+            if (moved.Y > 0f) {
+                Vector2 value3 = new Vector2(Left, Bottom - 1f);
+                for (int k = 0; k < Width; k += 4) {
+                    level.Particles.Emit(FinalBossMovingBlock.P_Stop, value3 + Vector2.UnitX * (2 + k + Calc.Random.Range(-1, 1)), direction);
+                }
+            } else if (moved.Y < 0f) {
+                Vector2 value4 = new Vector2(Left, Top);
+                for (int l = 0; l < Width; l += 4) {
+                    level.Particles.Emit(FinalBossMovingBlock.P_Stop, value4 + Vector2.UnitX * (2 + l + Calc.Random.Range(-1, 1)), direction);
+                }
+            }
+        }
+
+        private void ImpactParticles(Vector2 moved) {
+            if (moved.X < 0f) {
+                Vector2 offset = new Vector2(0f, 2f);
+                for (int i = 0; i < Height / 8f; i++) {
+                    Vector2 collideCheckPos = new Vector2(Left - 1f, Top + 4f + (i * 8));
+                    if (!Scene.CollideCheck<Water>(collideCheckPos) && Scene.CollideCheck<Solid>(collideCheckPos)) {
+                        SceneAs<Level>().ParticlesFG.Emit(CrushBlock.P_Impact, collideCheckPos + offset, 0f);
+                        SceneAs<Level>().ParticlesFG.Emit(CrushBlock.P_Impact, collideCheckPos - offset, 0f);
+                    }
+                }
+            } else if (moved.X > 0f) {
+                Vector2 offset = new Vector2(0f, 2f);
+                for (int j = 0; j < Height / 8f; j++) {
+                    Vector2 collideCheckPos = new Vector2(Right + 1f, Top + 4f + (j * 8));
+                    if (!Scene.CollideCheck<Water>(collideCheckPos) && Scene.CollideCheck<Solid>(collideCheckPos)) {
+                        SceneAs<Level>().ParticlesFG.Emit(CrushBlock.P_Impact, collideCheckPos + offset, (float) Math.PI);
+                        SceneAs<Level>().ParticlesFG.Emit(CrushBlock.P_Impact, collideCheckPos - offset, (float) Math.PI);
+                    }
+                }
+            }
+            if (moved.Y < 0f) {
+                Vector2 offset = new Vector2(2f, 0f);
+                for (int k = 0; k < Width / 8f; k++) {
+                    Vector2 collideCheckPos = new Vector2(Left + 4f + (k * 8), Top - 1f);
+                    if (!Scene.CollideCheck<Water>(collideCheckPos) && Scene.CollideCheck<Solid>(collideCheckPos)) {
+                        SceneAs<Level>().ParticlesFG.Emit(CrushBlock.P_Impact, collideCheckPos + offset, (float) Math.PI / 2f);
+                        SceneAs<Level>().ParticlesFG.Emit(CrushBlock.P_Impact, collideCheckPos - offset, (float) Math.PI / 2f);
+                    }
+                }
+            } else {
+                if (!(moved.Y > 0f)) {
+                    return;
+                }
+                Vector2 offset = new Vector2(2f, 0f);
+                for (int l = 0; l < Width / 8f; l++) {
+                    Vector2 collideCheckPos = new Vector2(Left + 4f + (l * 8), Bottom + 1f);
+                    if (!Scene.CollideCheck<Water>(collideCheckPos) && Scene.CollideCheck<Solid>(collideCheckPos)) {
+                        SceneAs<Level>().ParticlesFG.Emit(CrushBlock.P_Impact, collideCheckPos + offset, -(float) Math.PI / 2f);
+                        SceneAs<Level>().ParticlesFG.Emit(CrushBlock.P_Impact, collideCheckPos - offset, -(float) Math.PI / 2f);
+                    }
+                }
+            }
+        }
+    }
+}
