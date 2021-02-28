96df3855159983bf34ce43d76cd83f6e7ec2a7e1
bbb651 <bar.ye651@gmail.com>
2021-02-28 15:49:31 +0200

    Pufferfish dotted line effect

--- Entities/ExplodingStrawberry.cs
+++ Entities/ExplodingStrawberry.cs
@@ -1,7 +1,7 @@
 ﻿using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
 using Monocle;
-using MonoMod.Utils;
+using System;
 using System.Collections;
 
 namespace Celeste.Mod.StrawberryJam2021.Entities {
@@ -10,6 +10,7 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
     [Tracked]
     public class ExplodingStrawberry : Strawberry {
         private Sprite explosionSprite;
+        private Vector2 lastPlayerPos;
         public ExplodingStrawberry(EntityData data, Vector2 offset, EntityID gid) : base(data, offset, gid) { }
 
         public override void Added(Scene scene) {
@@ -17,6 +18,58 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
             explosionSprite = StrawberryJam2021Module.ExplodingStrawberrySpriteBank.Create("explodingStrawberry");
             explosionSprite.Visible = false;
             Add(explosionSprite);
+        }
+
+        public override void Update() {
+            base.Update();
+            Player entity = Scene.Tracker.GetEntity<Player>();
+            if (entity != null) {
+                lastPlayerPos = entity.Center;
+            }
+        }
+
+        public override void Render() {
+            // Taken and modified from Puffer.cs
+            float num1 = 1f;
+            if (explosionSprite.Visible) {
+                num1 = 1f - (float) explosionSprite.CurrentAnimationFrame / explosionSprite.CurrentAnimationTotalFrames;
+            }
+
+            if (num1 > 0.0) {
+                bool flag2 = false;
+                if (lastPlayerPos.Y < Y) {
+                    lastPlayerPos.Y = Y - (float) (((double) lastPlayerPos.Y - Y) * 0.5);
+                    lastPlayerPos.X += lastPlayerPos.X - X;
+                    flag2 = true;
+                }
+
+                float radiansB = (lastPlayerPos - Position).Angle();
+                for (int index = 0; index < 14; ++index) {
+                    float num2 = (float) Math.Sin(Scene.TimeActive * 0.5) * 0.02f;
+                    float num3 =
+                        Calc.Map(index / 14f + num2, 0.0f, 1f,
+                            0.10415082179037f, 3.246313f);
+                    Vector2 vector = Calc.AngleToVector(num3, 1f);
+                    Vector2 start = Position - new Vector2(0, 1.5f) + vector * 16f;
+                    if (num1 > 0.0) {
+                        if (index == 0 || index == 13) {
+                            Draw.Line(start, start - vector * 7.5f, Color.White * num1);
+                        } else {
+                            Vector2 distorted = vector * (float) Math.Sin(Scene.TimeActive * 2.0 +
+                                                                          index * 0.600000023841858);
+                            if (index % 2 == 0)
+                                distorted *= -1f;
+                            Vector2 translated = start + distorted;
+                            if (!flag2 && Calc.AbsAngleDiff(num3, radiansB) <= 0.174532920122147)
+                                Draw.Line(translated, translated - vector * 3f, Color.White * num1);
+                            else
+                                Draw.Point(translated, Color.White * num1);
+                        }
+                    }
+                }
+            }
+
+            base.Render();
         }
 
         public static void Load() {
