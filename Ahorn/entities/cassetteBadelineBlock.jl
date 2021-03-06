9f2524db75c871606a03615c594693ce6bb64c94
Moses Miller <Majora320@gmail.com>
2021-03-06 13:42:02 -0800

    Julia formatting

--- Ahorn/entities/cassetteBadelineBlock.jl
+++ Ahorn/entities/cassetteBadelineBlock.jl
@@ -2,49 +2,18 @@ module SJ2021CassetteBadelineBlock
 
 using ..Ahorn, Maple
 
-@pardef CassetteBadelineBlock(
-    x1::Integer,
-    y1::Integer,
-    x2::Integer = x1 + 16,
-    y2::Integer = y1,
-    width::Integer = Maple.defaultBlockWidth,
-    height::Integer = Maple.defaultBlockHeight,
-    tiletype::String = "g",
-    moveForwardBeat::Integer = 0,
-    moveBackBeat::Integer = 8,
-    preDelay::Integer = 0,
-    transitionDuration::Integer = 4,
-    oneWay::Bool = false,
-    teleportBack::Bool = false,
-) = Entity(
-    "SJ2021/CassetteBadelineBlock",
-    x = x1,
-    y = y1,
-    nodes = Tuple{Int,Int}[(x2, y2)],
-    width = width,
-    height = height,
-    tiletype = tiletype,
-    moveForwardBeat = moveForwardBeat,
-    moveBackBeat = moveBackBeat,
-    preDelay = preDelay,
-    transitionDuration = transitionDuration,
-    oneWay = oneWay,
-    teleportBack = teleportBack,
-)
+@pardef CassetteBadelineBlock(x1::Integer, y1::Integer, x2::Integer=x1 + 16, y2::Integer=y1,
+                              width::Integer=Maple.defaultBlockWidth, height::Integer=Maple.defaultBlockHeight,
+                              tiletype::String="g", moveForwardBeat::Integer=0, moveBackBeat::Integer=8,
+                              preDelay::Integer=0, transitionDuration::Integer=4, oneWay::Bool=false,
+                              teleportBack::Bool=false) =
+    Entity("SJ2021/CassetteBadelineBlock", x=x1, y=y1, nodes=Tuple{Int,Int}[(x2, y2)], width=width,
+           height=height, tiletype=tiletype, moveForwardBeat=moveForwardBeat, moveBackBeat=moveBackBeat,
+           preDelay=preDelay, transitionDuration=transitionDuration, oneWay=oneWay, teleportBack=teleportBack)
 
-Ahorn.editingOrder(entity::CassetteBadelineBlock) = String[
-    "x",
-    "y",
-    "width",
-    "height",
-    "moveForwardBeat",
-    "moveBackBeat",
-    "preDelay",
-    "transitionDuration",
-    "oneWay",
-    "teleportBack",
-    "tiletype",
-]
+Ahorn.editingOrder(entity::CassetteBadelineBlock) =
+    String["x", "y", "width", "height", "moveForwardBeat", "moveBackBeat", "preDelay", "transitionDuration",
+             "oneWay", "teleportBack", "tiletype"]
 
 const placements = Ahorn.PlacementDict(
     "Cassette-based Moving Block (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
@@ -52,12 +21,9 @@ const placements = Ahorn.PlacementDict(
         "rectangle",
         Dict{String,Any}(),
         function (entity)
-            entity.data["nodes"] = [(
-                Int(entity.data["x"]) + Int(entity.data["width"]) + 8,
-                Int(entity.data["y"]),
-            )]
-        end,
-    ),
+            entity.data["nodes"] = [(Int(entity.data["x"]) + Int(entity.data["width"]) + 8, Int(entity.data["y"]))]
+        end
+    )
 )
 
 Ahorn.editingOptions(entity::CassetteBadelineBlock) =
@@ -77,25 +43,11 @@ function Ahorn.selection(entity::CassetteBadelineBlock)
     return [Ahorn.Rectangle(x, y, width, height), Ahorn.Rectangle(nx, ny, width, height)]
 end
 
-function Ahorn.renderAbs(
-    ctx::Ahorn.Cairo.CairoContext,
-    entity::CassetteBadelineBlock,
-    room::Maple.Room,
-)
-    Ahorn.drawTileEntity(
-        ctx,
-        room,
-        entity,
-        material = get(entity.data, "tiletype", "g")[1],
-        blendIn = false,
-    )
+function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::CassetteBadelineBlock, room::Maple.Room)
+    Ahorn.drawTileEntity(ctx, room, entity, material = get(entity.data, "tiletype", "g")[1], blendIn = false)
 end
 
-function Ahorn.renderSelectedAbs(
-    ctx::Ahorn.Cairo.CairoContext,
-    entity::CassetteBadelineBlock,
-    room::Maple.Room,
-)
+function Ahorn.renderSelectedAbs(ctx::Ahorn.Cairo.CairoContext, entity::CassetteBadelineBlock, room::Maple.Room)
     x, y = Ahorn.position(entity)
     nodes = get(entity.data, "nodes", ())
 
@@ -106,34 +58,9 @@ function Ahorn.renderSelectedAbs(
         nx, ny = Int.(nodes[1])
         cox, coy = floor(Int, width / 2), floor(Int, height / 2)
 
-        fakeTiles = Ahorn.createFakeTiles(
-            room,
-            nx,
-            ny,
-            width,
-            height,
-            get(entity.data, "tiletype", "g")[1],
-            blendIn = false,
-        )
-        Ahorn.drawFakeTiles(
-            ctx,
-            room,
-            fakeTiles,
-            room.objTiles,
-            true,
-            nx,
-            ny,
-            clipEdges = true,
-        )
-        Ahorn.drawArrow(
-            ctx,
-            x + cox,
-            y + coy,
-            nx + cox,
-            ny + coy,
-            Ahorn.colors.selection_selected_fc,
-            headLength = 6,
-        )
+        fakeTiles = Ahorn.createFakeTiles(room, nx, ny, width, height, get(entity.data, "tiletype", "g")[1], blendIn = false)
+        Ahorn.drawFakeTiles(ctx, room, fakeTiles, room.objTiles, true, nx, ny, clipEdges = true)
+        Ahorn.drawArrow(ctx, x + cox, y + coy, nx + cox, ny + coy, Ahorn.colors.selection_selected_fc, headLength = 6)
     end
 end
 end
--- Ahorn/entities/cassetteConveyorBlock.jl
+++ Ahorn/entities/cassetteConveyorBlock.jl
@@ -2,48 +2,18 @@ module SJ2021CassetteConveyorBlock
 
 using ..Ahorn, Maple
 
-@pardef CassetteConveyorBlock(
-    x1::Integer,
-    y1::Integer,
-    x2::Integer = x1 + 16,
-    y2::Integer = y1,
-    x3::Integer = x2 + 16,
-    y3::Integer = y2,
-    width::Integer = Maple.defaultBlockWidth,
-    height::Integer = Maple.defaultBlockHeight,
-    tiletype::String = "g",
-    waitTime::Integer = 4,
-    preDelay::Integer = 0,
-    transitionDuration::Integer = 4,
-    ghostNodes::String = "",
-    teleportBack::Bool = true,
-) = Entity(
-    "SJ2021/CassetteConveyorBlock",
-    x = x1,
-    y = y1,
-    nodes = Tuple{Int,Int}[(x2, y2), (x3, y3)],
-    width = width,
-    height = height,
-    tiletype = tiletype,
-    preDelay = preDelay,
-    waitTime = waitTime,
-    transitionDuration = transitionDuration,
-    ghostNodes = ghostNodes,
-    teleportBack = teleportBack,
-)
+@pardef CassetteConveyorBlock(x1::Integer, y1::Integer, x2::Integer=x1 + 16, y2::Integer=y1,
+                              x3::Integer=x2 + 16, y3::Integer=y2, width::Integer=Maple.defaultBlockWidth,
+                              height::Integer=Maple.defaultBlockHeight, tiletype::String="g",
+                              waitTime::Integer=4, preDelay::Integer=0, transitionDuration::Integer=4,
+                              ghostNodes::String="", teleportBack::Bool=true) =
+    Entity("SJ2021/CassetteConveyorBlock", x = x1, y = y1, nodes = Tuple{Int,Int}[(x2, y2), (x3, y3)],
+           width = width,height = height, tiletype = tiletype, waitTime = waitTime, preDelay = preDelay,
+           transitionDuration = transitionDuration, ghostNodes = ghostNodes, teleportBack = teleportBack)
 
-Ahorn.editingOrder(entity::CassetteConveyorBlock) = String[
-    "x",
-    "y",
-    "width",
-    "height",
-    "transitionDuration",
-    "waitTime",
-    "preDelay",
-    "ghostNodes",
-    "tiletype",
-    "teleportBack",
-]
+Ahorn.editingOrder(entity::CassetteConveyorBlock) =
+    String["x", "y", "width", "height", "transitionDuration", "waitTime", "preDelay", "ghostNodes", 
+             "tiletype", "teleportBack"]
 
 const placements = Ahorn.PlacementDict(
     "Cassette-based Conveyor Block (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
@@ -51,18 +21,10 @@ const placements = Ahorn.PlacementDict(
         "rectangle",
         Dict{String,Any}(),
         function (entity)
-            entity.data["nodes"] = [
-                (
-                    Int(entity.data["x"]) + Int(entity.data["width"]) + 8,
-                    Int(entity.data["y"]),
-                ),
-                (
-                    Int(entity.data["x"]) + Int(entity.data["width"]) * 2 + 16,
-                    Int(entity.data["y"]),
-                ),
-            ]
-        end,
-    ),
+            entity.data["nodes"] = [(Int(entity.data["x"]) + Int(entity.data["width"]) + 8,     Int(entity.data["y"])),
+                                    (Int(entity.data["x"]) + Int(entity.data["width"]) * 2 + 16,Int(entity.data["y"]))]
+        end
+    )
 )
 
 Ahorn.editingOptions(entity::CassetteConveyorBlock) =
@@ -88,11 +50,7 @@ function Ahorn.selection(entity::CassetteConveyorBlock)
     return res
 end
 
-function Ahorn.renderAbs(
-    ctx::Ahorn.Cairo.CairoContext,
-    entity::CassetteConveyorBlock,
-    room::Maple.Room,
-)
+function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::CassetteConveyorBlock, room::Maple.Room)
     x, y = Ahorn.position(entity)
     nodes = get(entity.data, "nodes", ())
 
@@ -103,73 +61,24 @@ function Ahorn.renderAbs(
     ghostnodes = tryparse.(Int, split(ghosts, r", ?"))
 
     if 0 in ghostnodes
-        Ahorn.drawTileEntity(
-            ctx,
-            room,
-            entity,
-            material = get(entity.data, "tiletype", "g")[1],
-            alpha = 0.5,
-            blendIn = false,
-        )
+        Ahorn.drawTileEntity(ctx, room, entity, material = get(entity.data, "tiletype", "g")[1], alpha = 0.5, blendIn = false)
     else
-        Ahorn.drawTileEntity(
-            ctx,
-            room,
-            entity,
-            material = get(entity.data, "tiletype", "g")[1],
-            blendIn = false,
-        )
+        Ahorn.drawTileEntity(ctx, room, entity, material = get(entity.data, "tiletype", "g")[1], blendIn = false)
     end
 
     prev = (x, y)
     for (index, (nx, ny)) in enumerate(nodes)
         cox, coy = floor(Int, width / 2), floor(Int, height / 2)
 
-        fakeTiles = Ahorn.createFakeTiles(
-            room,
-            nx,
-            ny,
-            width,
-            height,
-            get(entity.data, "tiletype", "g")[1],
-            blendIn = false,
-        )
+        fakeTiles = Ahorn.createFakeTiles(room, nx, ny, width, height, get(entity.data, "tiletype", "g")[1], blendIn = false)
 
         if index in ghostnodes
-            Ahorn.drawFakeTiles(
-                ctx,
-                room,
-                fakeTiles,
-                room.objTiles,
-                true,
-                nx,
-                ny,
-                alpha = 0.5,
-                clipEdges = true,
-            )
+            Ahorn.drawFakeTiles(ctx, room, fakeTiles, room.objTiles, true, nx, ny, alpha = 0.5, clipEdges = true)
         else
-
-            Ahorn.drawFakeTiles(
-                ctx,
-                room,
-                fakeTiles,
-                room.objTiles,
-                true,
-                nx,
-                ny,
-                clipEdges = true,
-            )
+            Ahorn.drawFakeTiles(ctx, room, fakeTiles, room.objTiles, true, nx, ny, clipEdges = true)
         end
 
-        Ahorn.drawArrow(
-            ctx,
-            prev[1] + cox,
-            prev[2] + coy,
-            nx + cox,
-            ny + coy,
-            Ahorn.colors.selection_selected_fc,
-            headLength = 6,
-        )
+        Ahorn.drawArrow(ctx,prev[1] + cox,prev[2] + coy, nx + cox, ny + coy, Ahorn.colors.selection_selected_fc, headLength = 6)
         prev = (nx, ny)
     end
 end
