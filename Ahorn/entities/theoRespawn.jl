62972638a99ba96032c0276fd9416581492ff8fd
Brokemia <rrwthird@aol.com>
2021-03-14 14:52:42 -0400

    Add Theo Respawn

---
+++ Ahorn/entities/theoRespawn.jl
@@ -0,0 +1,23 @@
+﻿module SJ2021TheoRespawn
+
+using ..Ahorn, Maple
+
+@mapdef Entity "SJ2021/TheoRespawn" TheoRespawn(x::Integer, y::Integer)
+
+const placements = Ahorn.PlacementDict(
+    "Theo Respawn (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
+        TheoRespawn
+    )
+)
+
+sprite = "characters/theoCrystal/idle00.png"
+
+function Ahorn.selection(entity::TheoRespawn)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.getSpriteRectangle(sprite, x, y - 10)
+end
+
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::TheoRespawn, room::Maple.Room) = Ahorn.drawSprite(ctx, sprite, 0, -10, tint=((160, 160, 160, 70) ./ 255))
+
+end
