2fb72c08672e413550ae0808d041df9a42899b31
flipking78 <flipking0708@gmail.com>
2021-03-15 10:00:40 +0100

    add flag

--- Ahorn/triggers/bubbleEmitterFireTrigger.jl
+++ Ahorn/triggers/bubbleEmitterFireTrigger.jl
@@ -2,7 +2,7 @@ module SJ2021BubbleEmitterFireTrigger
 
 using ..Ahorn, Maple
 
-@mapdef Trigger "SJ2021/bubbleEmitterFireTrigger" BubbleEmitterFireTrigger(x::Integer, y::Integer, width::Integer=16, height::Integer=16)
+@mapdef Trigger "SJ2021/bubbleEmitterFireTrigger" BubbleEmitterFireTrigger(x::Integer, y::Integer, width::Integer=16, height::Integer=16, flag::String="")
 
 const placements = Ahorn.PlacementDict(
     "Bubble Emitter Fire Trigger\n(Strawberry Jam 2021)" => Ahorn.EntityPlacement(
