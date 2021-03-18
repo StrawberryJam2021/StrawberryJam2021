b33948f2e99eb4e95639e9ce1276fa70db660d1c
Kryn <78513664+Kryndolin@users.noreply.github.com>
2021-03-18 18:39:26 +0000

    Removed comments, fixed white space, marked variables as private.

--- Entities/PinkCloudyFlower.cs
+++ Entities/PinkCloudyFlower.cs
@@ -11,9 +11,9 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
 
     [CustomEntity("SJ2021/PinkCloudyFlower")]
     class PinkCloudyFlower : Glider {
-        Cloud cloud;
-        Vector2? oldPosition;
-        Vector2 carryOffset;
+        private Cloud cloud;
+        private Vector2? oldPosition;
+        private Vector2 carryOffset;
 
         public PinkCloudyFlower(EntityData data, Vector2 offset) : base(data, offset) {
             Get<Sprite>().SetColor(Color.MediumOrchid);
@@ -23,41 +23,30 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
 
         public override void Added(Scene scene) {
             base.Added(scene);
-            //cloud.Added(scene);
             scene.Add(cloud);
-            //scene.Tracker.EntityAdded(cloud);
-            //scene.TagLists.EntityAdded(cloud);
         }
 
         public override void Update() {
             base.Update();
-            //cloud.Update();
-            if (cloud.HasPlayerRider()) {
 
+            if (cloud.HasPlayerRider()) {
                 if (oldPosition == null) {
                     oldPosition = Position;
                 }
 
-                //Hold.Active = false;
                 Position = cloud.Position - carryOffset;
-                
             } else {
-
                 if (oldPosition != null) {
                     Position = oldPosition.Value;
                     oldPosition = null;
                 }
-                //Hold.Active = true;
+                
                 cloud.Position = Position + carryOffset;
             }
-
-            
         }
 
         public override void Render() {
             base.Render();
-            //cloud.Render();
         }
-        
     }
 }
