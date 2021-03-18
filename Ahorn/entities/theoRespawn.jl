b2c4bd0a5388dd02fb23381644960b70b162a8c6
Brokemia <rrwthird@aol.com>
2021-03-18 15:17:44 -0400

    Minor theo crystal Ahorn plugin changes

--- Ahorn/entities/theoRespawn.jl
+++ Ahorn/entities/theoRespawn.jl
@@ -10,7 +10,7 @@ const placements = Ahorn.PlacementDict(
     )
 )
 
-sprite = "characters/theoCrystal/idle00.png"
+sprite = "characters/theoCrystal/idle00"
 
 function Ahorn.selection(entity::TheoRespawn)
     x, y = Ahorn.position(entity)
@@ -18,6 +18,6 @@ function Ahorn.selection(entity::TheoRespawn)
     return Ahorn.getSpriteRectangle(sprite, x, y - 10)
 end
 
-Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::TheoRespawn, room::Maple.Room) = Ahorn.drawSprite(ctx, sprite, 0, -10, tint=((160, 160, 160, 70) ./ 255))
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::TheoRespawn) = Ahorn.drawSprite(ctx, sprite, 0, -10, tint=((160, 160, 160, 70) ./ 255))
 
 end
