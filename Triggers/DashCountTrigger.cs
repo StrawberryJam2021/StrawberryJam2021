6cc5cacaaa0618268c10f1cbbbf3a912850fcc0b
Thomasdb05 <78592830+Thomasdb05@users.noreply.github.com>
2021-03-21 20:21:23 +0100

    Now correctly turns of modded functions when u leave the map and should be all done

--- Triggers/DashCountTrigger.cs
+++ Triggers/DashCountTrigger.cs
@@ -12,17 +12,22 @@ namespace Celeste.Mod.StrawberryJam2021.Triggers {
     [Tracked]
     public class DashCountTrigger : Trigger {
 
-        bool HasSet = false;
+        private static bool HasCollided = false;
         int NumberOfDashes = 0;
+        private static bool IsInCurrentMap = false;
 
         public DashCountTrigger(EntityData data, Vector2 offset) : base(data, offset) {
             NumberOfDashes = data.Int("NumberOfDashes");
         }
 
         private static Color modPlayerGetHairColor(On.Celeste.PlayerHair.orig_GetHairColor orig, PlayerHair self, int index) {
-            Player player = (Player)self.Entity;
-            if (player.Dashes > 0) {
-                return Player.NormalHairColor;
+            if (self.Entity is Player) {
+                Player player = (Player) self.Entity;
+                if (player.Dashes > 0 && player != null && IsInCurrentMap) {
+                    return Player.NormalHairColor;
+                } else {
+                    return orig(self, 1);
+                }
             }
             else {
                 return orig(self, 1);
@@ -30,7 +35,7 @@ namespace Celeste.Mod.StrawberryJam2021.Triggers {
         }
 
         private static Color modPlayerGetTrailColor(On.Celeste.Player.orig_GetCurrentTrailColor orig, Player self) {
-            if (self.Dashes > 0) {
+            if (self.Dashes > 0 && IsInCurrentMap) {
                 return Player.NormalHairColor;
 
             } else {
@@ -38,27 +43,37 @@ namespace Celeste.Mod.StrawberryJam2021.Triggers {
             }
                
         }
+        public override void SceneEnd(Scene scene) {
+            base.SceneEnd(scene);
+        }
+
         public static void Load() {
             using (new DetourContext { After = { "*" } }) { 
                 On.Celeste.PlayerHair.GetHairColor += modPlayerGetHairColor;
                 On.Celeste.Player.GetCurrentTrailColor += modPlayerGetTrailColor;
             }
+            Everest.Events.Level.OnExit += modOnExit;
         }
         public static void Unload() {
             On.Celeste.PlayerHair.GetHairColor -= modPlayerGetHairColor;
             On.Celeste.Player.GetCurrentTrailColor -= modPlayerGetTrailColor;
+            Everest.Events.Level.OnExit -= modOnExit;
         }
 
-        public override void Awake(Scene scene) {
-            base.Awake(scene);
+        private static void modOnExit(Level level, LevelExit exit, LevelExit.Mode mode, Session session, HiresSnow snow) {
+            IsInCurrentMap = false;
+        }
+        public override void Added(Scene scene) {
+            base.Added(scene);
+            IsInCurrentMap = true;
         }
 
         public override void OnEnter(Player player) {
             base.OnEnter(player);
-            if (!HasSet) {
+            if (!HasCollided) {
                 SceneAs<Level>().Session.Inventory.Dashes = NumberOfDashes;
                 player.Dashes = NumberOfDashes;
-                HasSet = true;
+                HasCollided = true;
             }
         }
     }
