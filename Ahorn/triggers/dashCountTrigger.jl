659c98620258b76d0375f12f5995d8c58739ff4b
Thomasdb05 <78592830+Thomasdb05@users.noreply.github.com>
2021-03-22 23:43:35 +0100

    Fixed some bugs and now the deathparticle colors are right as wel
    SHOULD be done this time

--- Ahorn/triggers/dashCountTrigger.jl
+++ Ahorn/triggers/dashCountTrigger.jl
@@ -2,7 +2,7 @@ module SJ2021DashCountTrigger
 
 using ..Ahorn, Maple
 
-@mapdef Trigger "SJ2021/DashCountTrigger" DashCountTrigger(x::Integer, y::Integer, width::Integer=16, height::Integer=16, NumberOfDashes::Integer=1)
+@mapdef Trigger "SJ2021/DashCountTrigger" DashCountTrigger(x::Integer, y::Integer, width::Integer=16, height::Integer=16, NumberOfDashes::Integer=1, ResetOnDeath ::Bool = false)
 
 const placements = Ahorn.PlacementDict(
     "DashCount Trigger (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
--- Triggers/DashCountTrigger.cs
+++ Triggers/DashCountTrigger.cs
@@ -5,24 +5,30 @@ using Monocle;
 using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
 using System;
+using System.Collections;
+using System.Reflection;
+using MonoMod.Utils;
 
 namespace Celeste.Mod.StrawberryJam2021.Triggers {
 
         [CustomEntity("SJ2021/DashCountTrigger")]
     [Tracked]
     public class DashCountTrigger : Trigger {
-
-        private static bool HasCollided = false;
         int NumberOfDashes = 0;
         private static bool IsInCurrentMap = false;
+        private static int NormalDashAmount = 0;
+        private static bool ResetOnDeath = false;
+        bool ResetOnDeathPrivate = false;
+        private static Player player;
 
         public DashCountTrigger(EntityData data, Vector2 offset) : base(data, offset) {
             NumberOfDashes = data.Int("NumberOfDashes");
+            ResetOnDeathPrivate = data.Bool("ResetOnDeath");
         }
 
         private static Color modPlayerGetHairColor(On.Celeste.PlayerHair.orig_GetHairColor orig, PlayerHair self, int index) {
             if (self.Entity is Player) {
-                Player player = (Player) self.Entity;
+                player = (Player) self.Entity;
                 if (player.Dashes > 0 && player != null && IsInCurrentMap) {
                     return Player.NormalHairColor;
                 } else {
@@ -51,13 +57,30 @@ namespace Celeste.Mod.StrawberryJam2021.Triggers {
             using (new DetourContext { After = { "*" } }) { 
                 On.Celeste.PlayerHair.GetHairColor += modPlayerGetHairColor;
                 On.Celeste.Player.GetCurrentTrailColor += modPlayerGetTrailColor;
+                On.Celeste.Player.Die += modDie;
             }
             Everest.Events.Level.OnExit += modOnExit;
+            Everest.Events.Player.OnDie += modPlayerdie;
         }
         public static void Unload() {
             On.Celeste.PlayerHair.GetHairColor -= modPlayerGetHairColor;
             On.Celeste.Player.GetCurrentTrailColor -= modPlayerGetTrailColor;
-            Everest.Events.Level.OnExit -= modOnExit;
+            Everest.Events.Player.OnDie -= modPlayerdie;
+            On.Celeste.Player.Die -= modDie;
+        }
+
+        private static PlayerDeadBody modDie(On.Celeste.Player.orig_Die orig, Player self, Vector2 direction, bool evenIfInvincible = false, bool registerDeathInStats = true) {
+            if (IsInCurrentMap) {
+                PlayerDeadBody Deadbody = orig(self, Vector2.Zero, false, false);
+                if (player.Dashes > 0) {
+                    new DynData<PlayerDeadBody>(Deadbody)["initialHairColor"] = Player.NormalHairColor;
+                } else {
+                    new DynData<PlayerDeadBody>(Deadbody)["initialHairColor"] = Player.UsedHairColor;
+                }
+                return Deadbody;
+
+            } else
+                return orig(self, Vector2.Zero, false, false);
         }
 
         private static void modOnExit(Level level, LevelExit exit, LevelExit.Mode mode, Session session, HiresSnow snow) {
@@ -67,14 +90,21 @@ namespace Celeste.Mod.StrawberryJam2021.Triggers {
             base.Added(scene);
             IsInCurrentMap = true;
         }
+        private static void modPlayerdie(global::Celeste.Player player) {
+            if (ResetOnDeath) {
+                player.SceneAs<Level>().Session.Inventory.Dashes = NormalDashAmount;
+            }
+
+        }
 
         public override void OnEnter(Player player) {
             base.OnEnter(player);
-            if (!HasCollided) {
-                SceneAs<Level>().Session.Inventory.Dashes = NumberOfDashes;
-                player.Dashes = NumberOfDashes;
-                HasCollided = true;
-            }
+            player = player;
+            NormalDashAmount = SceneAs<Level>().Session.Inventory.Dashes;
+            SceneAs<Level>().Session.Inventory.Dashes = NumberOfDashes;
+            player.Dashes = NumberOfDashes;
+            ResetOnDeath = ResetOnDeathPrivate;
+            RemoveSelf();
         }
     }
 }
