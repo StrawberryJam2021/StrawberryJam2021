ac493ba45ba93632cd67e11e555b91e1199bebf5
Thomasdb05 <78592830+Thomasdb05@users.noreply.github.com>
2021-04-03 21:25:18 +0200

    Has correct hitboxes now?

--- Entities/UFO.cs
+++ Entities/UFO.cs
@@ -11,12 +11,40 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
     [CustomEntity("SJ2021/UFO")]
     public class UFO : FlingBird {
         EntityData entityData;
-        Sprite sprite;
+        Image sprite;
+        private Vector2 scale;
+        private SineWave idleSine;
+        private Wiggler bounceWiggler;
+        private Vector2 lastSinePosition;
+        private Vector2 anchorPosition;
         public UFO(Vector2[] nodes, bool skippable) : base(nodes, skippable) {
-            Add(new PlayerCollider(OnPlayer, new Hitbox(24f, 12f, 0f, -20f)));
+            scale = Vector2.One;
+            Add(new PlayerCollider(OnPlayer, new Hitbox(24f, 12f, -20f, -20f)));
+            base.Collider = new Hitbox(24f, 48f, -20f, 4f);
+            idleSine = new SineWave(0.5f, 0f);
+            idleSine.Randomize();
+            Add(idleSine);
+            Remove(Get<Sprite>());
+            Add(sprite = new Image(GFX.Game["objects/StrawberryJam2021/UFO/UFO"]));
+            sprite.CenterOrigin();
+            bounceWiggler = Wiggler.Create(0.6f, 2.5f, delegate (float v)
+            {
+                sprite.Rotation = v * 20f * ((float) Math.PI / 180f);
+            });
+            Add(bounceWiggler);
+            anchorPosition = Position;
         }
         public UFO(EntityData data, Vector2 levelOffset) : this(data.NodesWithPosition(levelOffset), data.Bool("waiting")) {
             entityData = data;
+        }
+
+        public override void Update() {
+            base.Update();
+            if (Position != lastSinePosition) {
+                anchorPosition += Position - lastSinePosition;
+            }
+            lastSinePosition = Position;
+
         }
 
         public override void Awake(Scene scene) {
@@ -34,18 +62,28 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
                     list[i].RemoveSelf();
                 }
             }
-            if (SegmentsWaiting[0]) {
-                sprite.Play("hoverStressed");
-                sprite.Scale.X = 1f;
-            }
             Player entity = scene.Tracker.GetEntity<Player>();
             if (entity != null && entity.X > base.X) {
                 RemoveSelf();
             }
         }
+
+        private void GotoHit(Vector2 from) {
+            scale = new Vector2(1.2f, 0.8f);
+            //hitSpeed = Vector2.UnitY * 200f;
+            bounceWiggler.Start();
+            //Alert(restart: true, playSfx: false);
+            Audio.Play("event:/new_content/game/10_farewell/puffer_boop", Position);
+        }
+
         private void OnPlayer(Player player) {
             if (player.Position.Y < Position.Y) {
                 player.Bounce(base.Top);
+                GotoHit(player.Center);
+               //MoveToX(anchorPosition.X);
+                idleSine.Reset();
+                anchorPosition = (lastSinePosition = Position);
+                //eyeSpin = 1f;
             }
 
         }
