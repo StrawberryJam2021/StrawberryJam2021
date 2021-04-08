7b074bbf0ab6c31fd11d8a128118c7b51edfd0be
Kryn <78513664+Kryndolin@users.noreply.github.com>
2021-04-08 01:33:49 +0100

    First implementation with JumpThru.

--- Entities/PinkCloudyFlower.cs
+++ Entities/PinkCloudyFlower.cs
@@ -11,37 +11,75 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
 
     [CustomEntity("SJ2021/PinkCloudyFlower")]
     class PinkCloudyFlower : Glider {
-        private Cloud cloud;
-        private Vector2? oldPosition;
+        private JumpThru platform;
         private Vector2 carryOffset;
+        private Holdable hold;
+        private bool waiting, returning;
+        private float speed;
 
         public PinkCloudyFlower(EntityData data, Vector2 offset) : base(data, offset) {
             Get<Sprite>().SetColor(Color.MediumOrchid);
-            cloud = new Cloud(Position, false);
-            carryOffset = new Vector2(0, -20);
+            carryOffset = new Vector2(-16, -20);
+            hold = Get<Holdable>();
+            platform = new JumpThru(Position + carryOffset, 32, false);
         }
 
         public override void Added(Scene scene) {
             base.Added(scene);
-            scene.Add(cloud);
+            scene.Add(platform);
         }
 
         public override void Update() {
             base.Update();
 
-            if (cloud.HasPlayerRider()) {
-                if (oldPosition == null) {
-                    oldPosition = Position;
-                }
+            if (platform.GetPlayerRider() == null)
+            {
+                hold.Active = true;
+            }
 
-                Position = cloud.Position - carryOffset;
-            } else {
-                if (oldPosition != null) {
-                    Position = oldPosition.Value;
-                    oldPosition = null;
+            platformUpdate();
+        }
+
+        private void platformUpdate() {
+            if (waiting) {
+                Player playerRider = platform.GetPlayerRider();
+                if (playerRider != null && playerRider.Speed.Y >= 0f) {
+                    //this.canRumble = true;
+                    hold.Active = false;
+                    speed = 180f;
+                    waiting = false;
+                    Audio.Play("event:/game/04_cliffside/cloud_blue_boost", Position);
+                    return;
                 }
-                
-                cloud.Position = Position + carryOffset;
+                platform.MoveTo(Position + carryOffset);
+            } else if (returning) {
+                speed = Calc.Approach(speed, 180f, 600f * Engine.DeltaTime);
+                platform.MoveTowardsY(Position.Y + carryOffset.Y, speed * Engine.DeltaTime);
+                if (platform.ExactPosition.Y == Position.Y + carryOffset.Y) {
+                    returning = false;
+                    waiting = true;
+                    speed = 0f;
+                    return;
+                }
+            } else {
+                if (platform.Y >= Position.Y + carryOffset.Y) {
+                    speed -= 1200f * Engine.DeltaTime;
+                } else {
+                    speed += 1200f * Engine.DeltaTime;
+                    if (speed >= -100f) {
+                        Player playerRider2 = platform.GetPlayerRider();
+                        if (playerRider2 != null && playerRider2.Speed.Y >= 0f) {
+                            playerRider2.Speed.Y = -200f;
+                            hold.Active = false;
+                        }
+                        returning = true;
+                    }
+                }
+                float num = speed;
+                if (num < 0f) {
+                    num = -220f;
+                }
+                platform.MoveV(speed * Engine.DeltaTime, num);
             }
         }
 
