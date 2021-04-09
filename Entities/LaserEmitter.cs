0a5246cb65a327adb3c1cc55cfd36e027b58d2b4
Shane Woolcock <samahnub@gmail.com>
2021-04-09 10:22:38 +0930

    Add laser disable and linked zip mover functionalities

--- Entities/LaserEmitter.cs
+++ Entities/LaserEmitter.cs
@@ -2,6 +2,7 @@ using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
 using Monocle;
 using System;
+using System.Linq;
 
 namespace Celeste.Mod.StrawberryJam2021.Entities {
     /// <summary>
@@ -17,6 +18,8 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
     /// <item><description>"flicker" =&gt; <see cref="Flicker"/></description></item>
     /// <item><description>"thickness" =&gt; <see cref="Thickness"/></description></item>
     /// <item><description>"killPlayer" =&gt; <see cref="KillPlayer"/></description></item>
+    /// <item><description>"disableLasers" =&gt; <see cref="DisableLasers"/></description></item>
+    /// <item><description>"triggerZipMovers" =&gt; <see cref="TriggerZipMovers"/></description></item>
     /// </list>
     /// </remarks>
     [CustomEntity("SJ2021/LaserEmitterUp = LoadUp",
@@ -101,12 +104,29 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
         /// </remarks>
         public bool KillPlayer { get; }
         
+        /// <summary>
+        /// Whether or not colliding with this beam will disable all beams of the same color.
+        /// </summary>
+        /// <remarks>
+        /// Defaults to false.
+        /// </remarks>
+        public bool DisableLasers { get; }
+        
+        /// <summary>
+        /// Whether or not colliding with this beam will trigger AdventureHelper LinkedZipMovers of the same color.
+        /// </summary>
+        /// <remarks>
+        /// Defaults to false.
+        /// </remarks>
+        public bool TriggerZipMovers { get; }
+
         #endregion
         
         #region Private Fields
         
         private const float flickerFrequency = 4f;
         private const float flickerRange = 4f;
+        private const float triggerCooldown = 1f;
 
         private readonly StaticMover staticMover;
         private readonly Sprite emitterSprite;
@@ -115,6 +135,9 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
         
         private Vector2 target;
         private float currentAlpha;
+        private float triggerCooldownRemaining = 0f;
+
+        private readonly string hexColor;
         
         #endregion
         
@@ -123,11 +146,14 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
             Orientation = orientation;
             
             // read properties from EntityData
-            Color = data.HexColor("color", Color.Red);
+            hexColor = data.Attr("color", "FF0000").ToLower();
+            Color = Calc.HexToColor(hexColor);
             Flicker = data.Bool("flicker", true);
             Thickness = Math.Max(data.Float("thickness", 6f), 0f);
             currentAlpha = Alpha = Calc.Clamp(data.Float("alpha", 0.4f), 0f, 1f);
             KillPlayer = data.Bool("killPlayer", true);
+            DisableLasers = data.Bool("disableLasers");
+            TriggerZipMovers = data.Bool("triggerZipMovers");
             
             // same depth as springs
             Depth = Depths.Above - 1;
@@ -163,20 +189,46 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
         
         public override void Update() {
             base.Update();
+
+            if (triggerCooldownRemaining > 0)
+                triggerCooldownRemaining -= triggerCooldown * Engine.DeltaTime;
             
             updateBeam();
         }
 
-        private void onPlayerCollide(Player player) {
-            if (!KillPlayer) return;
+        public void Disable() {
+            // TODO: nice animations
+            killZoneRect.Visible = false;
+            Collidable = false;
+        }
 
-            Vector2 direction;
-            if (Orientation == Orientations.Left || Orientation == Orientations.Right)
-                direction = player.Position.Y <= Position.Y ? -Vector2.UnitY : Vector2.UnitY;
-            else
-                direction = player.Position.X <= Position.X ? -Vector2.UnitX : Vector2.UnitX;
-            
-            player.Die(direction);
+        private void onPlayerCollide(Player player) {
+            var level = player.SceneAs<Level>();
+
+            if (triggerCooldownRemaining <= 0) {
+                triggerCooldownRemaining = triggerCooldown;
+                
+                if (DisableLasers) {
+                    var emitters = level.Entities.OfType<LaserEmitter>().Where(e => e.hexColor == hexColor);
+                    foreach (var emitter in emitters)
+                        emitter.Disable();
+                }
+
+                if (TriggerZipMovers) {
+                    string syncFlagCode = $"ZipMoverSync:{hexColor}";
+                    level.Session.SetFlag(syncFlagCode);
+                }
+            }
+
+            if (KillPlayer) {
+                Vector2 direction;
+                if (Orientation == Orientations.Left || Orientation == Orientations.Right)
+                    direction = player.Position.Y <= Position.Y ? -Vector2.UnitY : Vector2.UnitY;
+                else
+                    direction = player.Position.X <= Position.X ? -Vector2.UnitX : Vector2.UnitX;
+
+                player.Die(direction);
+            }
         }
 
         private void resizeKillbox(float size) {
