f8b0517ebe70b88a13b60b28ea0092ba78027249
Thomasdb05 <78592830+Thomasdb05@users.noreply.github.com>
2021-04-21 23:04:31 +0200

    Fixed the sprite names in the Ahorn plugin
    I fixed the sprite names in the Ahorn plugin, also corrected some of the indents and added (Strawberry Jam 2021) to my placement name.

--- Ahorn/entities/UFO.jl
+++ Ahorn/entities/UFO.jl
@@ -2,38 +2,37 @@ module SJ2021UFO
 
 using ..Ahorn, Maple
 
-@mapdef Entity "SJ2021/UFO" UFO(x::Integer, y::Integer, nodes::Array{Tuple{Integer, Integer}, 1}=Tuple{Integer, Integer}[], RaySizeX::Integer = 13, RaySizeY::Integer = 60)
+@mapdef Entity(Strawberry Jam 2021) "SJ2021/UFO" UFO(x::Integer, y::Integer, nodes::Array{Tuple{Integer, Integer}, 1}=Tuple{Integer, Integer}[], RaySizeX::Integer = 13, RaySizeY::Integer = 60)
 
 const placements = Ahorn.PlacementDict(
-    "UFO" => Ahorn.EntityPlacement(
+    "UFO (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
         UFO
     )				
 )
 
+UFOsprite = "objects/StrawberryJam2021/UFO/UFO.png"
+
 function Ahorn.selection(entity::UFO)
     x, y = Ahorn.position(entity)
     nodes = get(entity.data, "nodes", ())
-    spinnersprite = "objects/StrawberryJam2021/UFO/UFO.png"
     res = [Ahorn.Rectangle(x - 13, y - 21, 26, 26)]
 	
-	for node in nodes
+    for node in nodes
         nx, ny = node
-        push!(res, Ahorn.getSpriteRectangle(spinnersprite, nx, ny))
+        push!(res, Ahorn.getSpriteRectangle(UFOsprite, nx, ny))
 	end
 	
 	return res
 end
 
-spinnersprite = "objects/StrawberryJam2021/UFO/UFO.png"
 function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::UFO, room::Maple.Room)
     nodes = get(entity.data, "nodes", ())
     for node in nodes
         nx, ny = node
-        Ahorn.drawSprite(ctx, spinnersprite, nx, ny)
+        Ahorn.drawSprite(ctx, UFOsprite, nx, ny)
     end
-	sprite = "objects/StrawberryJam2021/UFO/UFO.png"
-x, y = Ahorn.position(entity)
-Ahorn.drawSprite(ctx, sprite, x, y)
+    x, y = Ahorn.position(entity)
+    Ahorn.drawSprite(ctx, UFOsprite, x, y)
 end
 Ahorn.nodeLimits(entity::UFO) = 1, -1
 end
