13271cd0a518ccb2f35f01172bd94d2bb1a6420d
Thomasdb05 <78592830+Thomasdb05@users.noreply.github.com>
2021-04-28 09:25:42 +0200

    now use tracker to find player
    It now uses tracker to find player, instead of just scene.entities.findfirst.

--- Triggers/DashCountTrigger.cs
+++ Triggers/DashCountTrigger.cs
@@ -94,7 +94,7 @@ namespace Celeste.Mod.StrawberryJam2021.Triggers {
 
         private static void modDraw(On.Celeste.DeathEffect.orig_Draw orig, Vector2 position, Color color, float ease) {
             if (player == null) {
-                player = scene.Entities.FindFirst<Player>();
+                player = scene.Tracker.GetEntity<Player>();
             }
             if (IsInCurrentMap && player != null) {
                 if (player.Dashes > 0) {
