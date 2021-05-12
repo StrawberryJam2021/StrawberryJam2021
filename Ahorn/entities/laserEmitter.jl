7fa2579aa23938506b35523f7938c0034c9ebe3c
Shane Woolcock <samahnub@gmail.com>
2021-05-12 18:34:42 +0930

    Add tinted laser emitter sprite

--- Ahorn/entities/laserEmitter.jl
+++ Ahorn/entities/laserEmitter.jl
@@ -4,41 +4,51 @@ using ..Ahorn, Maple
 
 const default_alpha = 0.4
 const default_thickness = 6.0
-const default_color = "FF0000"
+const default_style = "Tinted"
+
+# color is used to render the beam and tint the emitter
+# FF2626 (HSV 0,85,100) is the default red as requested by hennyburgr/SchaapMakker 
+const default_color = "FF2626"
+
+# color channel is used for linking lasers with each other and with linkedzipmovers
+# defaults to fullbright red as a matter of convenience
+const default_color_channel = "FF0000"
+
+const styles = ["Simple", "Tinted"]
 
 @mapdef Entity "SJ2021/LaserEmitterUp" LaserEmitterUp(
     x::Integer, y::Integer,
     color::String=default_color, alpha::Real=default_alpha, thickness::Real=default_thickness, flicker::Bool=true,
-    killPlayer::Bool=true, disableLasers::Bool=false, triggerZipMovers::Bool=false, collideWithSolids::Bool=true
+    killPlayer::Bool=true, collideWithSolids::Bool=true,
+    disableLasers::Bool=false, triggerZipMovers::Bool=false, colorChannel::String=default_color_channel,
+    style::String=default_style
 )
 
 @mapdef Entity "SJ2021/LaserEmitterDown" LaserEmitterDown(
     x::Integer, y::Integer,
     color::String=default_color, alpha::Real=default_alpha, thickness::Real=default_thickness, flicker::Bool=true,
-    killPlayer::Bool=true, disableLasers::Bool=false, triggerZipMovers::Bool=false, collideWithSolids::Bool=true
+    killPlayer::Bool=true, collideWithSolids::Bool=true,
+    disableLasers::Bool=false, triggerZipMovers::Bool=false, colorChannel::String=default_color_channel,
+    style::String=default_style
 )
 
 @mapdef Entity "SJ2021/LaserEmitterLeft" LaserEmitterLeft(
     x::Integer, y::Integer,
     color::String=default_color, alpha::Real=default_alpha, thickness::Real=default_thickness, flicker::Bool=true,
-    killPlayer::Bool=true, disableLasers::Bool=false, triggerZipMovers::Bool=false, collideWithSolids::Bool=true
+    killPlayer::Bool=true, collideWithSolids::Bool=true,
+    disableLasers::Bool=false, triggerZipMovers::Bool=false, colorChannel::String=default_color_channel,
+    style::String=default_style
 )
 
 @mapdef Entity "SJ2021/LaserEmitterRight" LaserEmitterRight(
     x::Integer, y::Integer,
     color::String=default_color, alpha::Real=default_alpha, thickness::Real=default_thickness, flicker::Bool=true,
-    killPlayer::Bool=true, disableLasers::Bool=false, triggerZipMovers::Bool=false, collideWithSolids::Bool=true
+    killPlayer::Bool=true, collideWithSolids::Bool=true,
+    disableLasers::Bool=false, triggerZipMovers::Bool=false, colorChannel::String=default_color_channel,
+    style::String=default_style
 )
 
-const colors = Dict{String, String}(
-    "Red" => "FF0000",
-    "Green" => "00FF00",
-    "Blue" => "0000FF",
-    "Cyan" => "00FFFF",
-    "Magenta" => "FF00FF",
-    "Yellow" => "FFFF00",
-    "White" => "FFFFFF"
-)
+const laserEmitterUnion = Union{LaserEmitterUp, LaserEmitterDown, LaserEmitterLeft, LaserEmitterRight}
 
 const placements = Ahorn.PlacementDict(
     "Laser Emitter (Up) (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
@@ -75,16 +85,13 @@ function Ahorn.selection(entity::LaserEmitterRight)
     return Ahorn.Rectangle(x, y - 7, 8, 14)
 end
 
-Ahorn.editingOptions(entity::LaserEmitterUp) = Dict{String, Any}( "color" => colors )
-Ahorn.editingOptions(entity::LaserEmitterDown) = Dict{String, Any}( "color" => colors )
-Ahorn.editingOptions(entity::LaserEmitterLeft) = Dict{String, Any}( "color" => colors )
-Ahorn.editingOptions(entity::LaserEmitterRight) = Dict{String, Any}( "color" => colors )
+simple_sprite = "objects/StrawberryJam2021/laserEmitter/simple00"
 
-sprite = "objects/StrawberryJam2021/laserEmitter/idle00"
+Ahorn.editingOptions(entity::laserEmitterUnion) = Dict{String, Any}( "style" => styles )
 
-Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::LaserEmitterUp, room::Maple.Room) = Ahorn.drawSprite(ctx, sprite, 0, 0, jx=0.5, jy=1, rot=0)
-Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::LaserEmitterDown, room::Maple.Room) = Ahorn.drawSprite(ctx, sprite, 16, 16, jx=0.5, jy=1, rot=pi)
-Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::LaserEmitterLeft, room::Maple.Room) = Ahorn.drawSprite(ctx, sprite, 0, 16, jx=0.5, jy=1, rot=-pi/2)
-Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::LaserEmitterRight, room::Maple.Room) = Ahorn.drawSprite(ctx, sprite, 16, 0, jx=0.5, jy=1, rot=pi/2)
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::LaserEmitterUp, room::Maple.Room) = Ahorn.drawSprite(ctx, simple_sprite, 0, 0, jx=0.5, jy=1, rot=0)
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::LaserEmitterDown, room::Maple.Room) = Ahorn.drawSprite(ctx, simple_sprite, 16, 16, jx=0.5, jy=1, rot=pi)
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::LaserEmitterLeft, room::Maple.Room) = Ahorn.drawSprite(ctx, simple_sprite, 0, 16, jx=0.5, jy=1, rot=-pi/2)
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::LaserEmitterRight, room::Maple.Room) = Ahorn.drawSprite(ctx, simple_sprite, 16, 0, jx=0.5, jy=1, rot=pi/2)
 
 end
--- Entities/LaserEmitter.cs
+++ Entities/LaserEmitter.cs
@@ -8,7 +8,7 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
     /// Entity that emits a flickering laser beam.
     /// </summary>
     /// <remarks>
-    /// Opacity of the beam edges is calculated as <see cref="Alpha"/> times <see cref="alphaMultiplier"/>, where
+    /// Opacity of the beam edges is calculated as <see cref="Alpha"/> times a dynamic multiplier, where
     /// the multiplier represents an optional flickering based on a <see cref="SineWave"/>.
     /// The centre third of the beam is twice that opacity.<br/>
     /// 
@@ -25,6 +25,9 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
     /// <item><term>color</term><description>
     /// The base <see cref="Microsoft.Xna.Framework.Color"/> used to render the beam.
     /// Defaults to <see cref="Microsoft.Xna.Framework.Color.Red"/>.
+    /// </description></item>
+    /// <item><term>colorChannel</term><description>
+    /// The color hex code used to match lasers with LinkedZipMovers.
     /// </description></item>
     /// <item><term>disableLasers</term><description>
     /// Whether or not colliding with this beam will disable all beams of the same color.
@@ -91,9 +94,18 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
         /// The base <see cref="Microsoft.Xna.Framework.Color"/> used to render the beam.
         /// </summary>
         /// <remarks>
-        /// Defaults to <see cref="Microsoft.Xna.Framework.Color.Red"/>.
+        /// If not set, will attempt to use the value of <see cref="ColorChannel"/>,
+        /// otherwise defaults to <see cref="Microsoft.Xna.Framework.Color.Red"/>.
         /// </remarks>
         public Color Color { get; protected set; }
+        
+        /// <summary>
+        /// The color hex code used to match lasers with LinkedZipMovers.
+        /// </summary>
+        /// <remarks>
+        /// If not set, will use the hex representation of the current <see cref="Color"/>.
+        /// </remarks>
+        public string ColorChannel { get; }
         
         /// <summary>
         /// Whether or not colliding with this beam will disable all beams of the same color.
@@ -120,6 +132,14 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
         public bool KillPlayer { get; }
         
         /// <summary>
+        /// The rendering style to use.
+        /// </summary>
+        /// <remarks>
+        /// Defaults to "Tinted".
+        /// </remarks>
+        public EmitterStyle Style { get; }
+        
+        /// <summary>
         /// The thickness of the beam (and corresponding <see cref="Hitbox"/> in pixels).
         /// </summary>
         /// <remarks>
@@ -141,37 +161,72 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
         
         private const float triggerCooldown = 1f;
         private const float flickerFrequency = 4f;
-        private const float flickerRange = 4f;
-        
-        private readonly string hexColor;
-        
+        private const float beamFlickerRange = 0.25f;
+        private const float emitterFlickerRange = 0.15f;
+
         private float triggerCooldownRemaining;
-        private float alphaMultiplier = 1f;
+        private float sineValue;
+
+        private Sprite emitterSprite;
+        private Sprite tintSprite;
         
         #endregion
         
         public LaserEmitter(EntityData data, Vector2 offset, Orientations orientation)
             : base(data, offset, orientation) {
+            string colorString = data.Attr("color", null);
+            string colorChannelString = data.Attr("colorChannel", null);
+            colorString ??= colorChannelString ?? "ff0000";
+            colorChannelString ??= colorString;
+
             Alpha = Calc.Clamp(data.Float("alpha", 0.4f), 0f, 1f);
             CollideWithSolids = data.Bool("collideWithSolids", true);
-            Color = Calc.HexToColor(hexColor = data.Attr("color", "FF0000").ToLower());
+            Color = Calc.HexToColor(colorString.ToLower());
+            ColorChannel = colorChannelString.ToLower();
             DisableLasers = data.Bool("disableLasers");
             Flicker = data.Bool("flicker", true);
             KillPlayer = data.Bool("killPlayer", true);
+            Style = data.Enum("style", EmitterStyle.Tinted);
             Thickness = Math.Max(data.Float("thickness", 6f), 0f);
             TriggerZipMovers = data.Bool("triggerZipMovers");
 
             Add(new PlayerCollider(onPlayerCollide),
                 new LaserColliderComponent {CollideWithSolids = CollideWithSolids, Thickness = Thickness,},
-                new SineWave(flickerFrequency) {OnUpdate = v => alphaMultiplier = 1 - (v + 1f) * 0.5f / flickerRange}
+                new SineWave(flickerFrequency) {OnUpdate = v => sineValue = v}
             );
             
             Collider = Get<LaserColliderComponent>().Collider;
-            
-            Sprite emitterSprite = StrawberryJam2021Module.SpriteBank.Create("laserEmitter");
+
+            switch (Style) {
+                default:
+                case EmitterStyle.Simple:
+                    addSimpleSprite();
+                    break;
+                
+                case EmitterStyle.Tinted:
+                    addTintedSprites();
+                    break;
+            }
+        }
+
+        private void addSimpleSprite() {
+            emitterSprite = StrawberryJam2021Module.SpriteBank.Create("laserEmitter");
+            emitterSprite.Play("simple");
             emitterSprite.Rotation = Orientation.Angle();
-            
             Add(emitterSprite);
+        }
+
+        private void addTintedSprites() {
+            emitterSprite = StrawberryJam2021Module.SpriteBank.Create("laserEmitter");
+            emitterSprite.Play("tinted_base");
+            emitterSprite.Rotation = Orientation.Angle();
+            Add(emitterSprite);
+            
+            tintSprite = StrawberryJam2021Module.SpriteBank.Create("laserEmitter");
+            tintSprite.Play("tinted_tint");
+            tintSprite.Color = Color;
+            tintSprite.Rotation = Orientation.Angle();
+            Add(tintSprite);
         }
 
         public override void Update() {
@@ -184,6 +239,7 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
         public override void Render() {
             // only render beam if we're collidable
             if (Collidable) {
+                float alphaMultiplier = 1f - (sineValue + 1f) * 0.5f * beamFlickerRange;
                 var color = Color * Alpha * (Flicker ? alphaMultiplier : 1f);
 
                 Draw.Rect(Collider.Bounds, color);
@@ -202,6 +258,19 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
 
                 Draw.Line(X, Y, X + target.X, Y + target.Y, color, lineThickness);
             }
+            
+            // update tint layer based on multiplier and collision
+            if (tintSprite != null) {
+                Color color;
+                if (!Collidable)
+                    color = Color.Gray;
+                else {
+                    float alphaMultiplier = 1f - (sineValue + 1f) * 0.5f * emitterFlickerRange; 
+                    color = Color * (Flicker ? alphaMultiplier : 1f);
+                }
+                color.A = 255;
+                tintSprite.Color = color;
+            }
 
             // render the emitter etc. after the beam
             base.Render();
@@ -215,13 +284,13 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
                 
                 if (DisableLasers) {
                     level.Entities.With<LaserEmitter>(emitter => {
-                        if (emitter.Color == Color)
+                        if (emitter.ColorChannel == ColorChannel)
                             emitter.Collidable = false;
                     });
                 }
 
                 if (TriggerZipMovers) {
-                    string syncFlagCode = $"ZipMoverSync:{hexColor}";
+                    string syncFlagCode = $"ZipMoverSync:{ColorChannel}";
                     level.Session.SetFlag(syncFlagCode);
                 }
             }
@@ -235,6 +304,11 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
 
                 player.Die(direction);
             }
+        }
+
+        public enum EmitterStyle {
+            Simple,
+            Tinted,
         }
     }
 }
