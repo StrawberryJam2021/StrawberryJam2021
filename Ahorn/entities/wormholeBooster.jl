08588d96e5deecf41517dcc4b31820437f3709a5
aridai-shi <jakubkaczma5@gmail.com>
2021-05-15 08:16:26 +0200

    Changed coloring to a constant
    Using hex was more convenient, but probably more resource intensive, so I manually converted the color.

--- Ahorn/entities/wormholeBooster.jl
+++ Ahorn/entities/wormholeBooster.jl
@@ -3,9 +3,7 @@ module SJ2021WormholeBooster
 using ..Ahorn, Maple
 
 @mapdef Entity "SJ2021/WormholeBooster" WormholeBooster(x::Integer, y::Integer)
-function getColor()
-    return ((Ahorn.argb32ToRGBATuple(parse(Int, "7800bd", base=16))[1:3] ./ 255)..., 1.0)
-end
+
 
 const placements = Ahorn.PlacementDict(
     "Wormhole Booster (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
@@ -13,7 +11,7 @@ const placements = Ahorn.PlacementDict(
         "rectangle"
     )
 )
-
+const boosterColor = (120,0,189, 1) ./ (255, 255, 255, 1)
 function Ahorn.selection(entity::WormholeBooster)
     x, y = Ahorn.position(entity)
     sprite = "objects/StrawberryJam2021/boosterWormhole/boosterWormhole00"
@@ -24,7 +22,7 @@ end
 function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::WormholeBooster, room::Maple.Room)
     sprite = "objects/StrawberryJam2021/boosterWormhole/boosterWormhole00"
 
-    Ahorn.drawSprite(ctx, sprite, 0, 0, tint=tint=getColor())
+    Ahorn.drawSprite(ctx, sprite, 0, 0, tint=boosterColor)
 end
 
 end
