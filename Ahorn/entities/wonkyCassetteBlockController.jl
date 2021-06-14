a088e17e126d3724d6e7e575c26cbcd1177d3067
catapillie <espritox@gmail.com>
2021-06-14 22:29:54 +0200

    Added specific controller placements for SJ2021 mappers

--- Ahorn/entities/wonkyCassetteBlockController.jl
+++ Ahorn/entities/wonkyCassetteBlockController.jl
@@ -15,6 +15,36 @@ const placements = Ahorn.PlacementDict(
     "Wonky Cassette Block Controller (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
         WonkyCassetteBlockController,
     ),
+    "Wonky Cassette Block Controller\n(116 BPM, 16 bars of 7/8, \"78_eighth_note\") (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
+        WonkyCassetteBlockController,
+        "point",
+        Dict{String, Any}(
+            "bpm" => 116,
+            "bars" => 16,
+            "timeSignature" => "7/8",
+            "sixteenthNoteParam" => "78_eighth_note",
+        )
+    ),
+    "Wonky Cassette Block Controller\n(116 BPM, 6 bars of 10/8, \"108_eighth_note\") (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
+        WonkyCassetteBlockController,
+        "point",
+        Dict{String, Any}(
+            "bpm" => 116,
+            "bars" => 6,
+            "timeSignature" => "10/8",
+            "sixteenthNoteParam" => "108_eighth_note",
+        )
+    ),
+    "Wonky Cassette Block Controller\n(174 BPM, 8 bars of 13/8, \"138_eighth_note\") (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
+        WonkyCassetteBlockController,
+        "point",
+        Dict{String, Any}(
+            "bpm" => 174,
+            "bars" => 8,
+            "timeSignature" => "13/8",
+            "sixteenthNoteParam" => "138_eighth_note",
+        )
+    ),
 )
 
 const sprite = "objects/StrawberryJam2021/wonkyCassetteBlockController/icon"
