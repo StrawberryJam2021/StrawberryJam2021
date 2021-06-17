fcca64d0ad1d27a824711bd4b1cb3ad432a97099
max4805 <52103563+max4805@users.noreply.github.com>
2021-06-17 18:39:18 +0200

    Update Entities/BulletTimeController.cs

--- Entities/BulletTimeController.cs
+++ Entities/BulletTimeController.cs
@@ -4,24 +4,24 @@ using Microsoft.Xna.Framework;
 using Monocle;
 
 [CustomEntity("SJ2021/BTController")]
-
-
 public class BTController : Entity {
     float timerate = 1;
+
     public BTController(EntityData data, Vector2 offset) : base(data.Position + offset) {
         timerate = data.Float("speed");
     }
+
     public override void Update() {
         base.Update();
         Player player = Scene.Tracker.GetEntity<Player>();
         if (player != null) {
-            if (player.Dashes == 0 || player.IsIntroState == true || player.JustRespawned == true) {
-                Engine.TimeRate = 1.0F;
+            if (player.Dashes == 0 || player.IsIntroState || player.JustRespawned) {
+                Engine.TimeRate = 1.0f;
             } else {
                 Engine.TimeRate = timerate;
             }
-        } else
-            Engine.TimeRate = 1.0F;
+        } else {
+            Engine.TimeRate = 1.0f;
+        }
     }
 }
-
