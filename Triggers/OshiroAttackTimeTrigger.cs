fdc7cfbb3e8b6decdd082d6c7540d43b77d40c77
aridai-shi <jakubkaczma5@gmail.com>
2021-06-19 23:08:23 +0200

    Fixed the Oshiro Attack Time Trigger

--- Triggers/OshiroAttackTimeTrigger.cs
+++ Triggers/OshiroAttackTimeTrigger.cs
@@ -10,29 +10,38 @@ using Microsoft.Xna.Framework;
 using Mono.Cecil.Cil;
 using Celeste.Mod.Entities;
 using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
+using System;
+using System.Collections;
+using System.Reflection;
+using MonoMod.Cil;
 
 namespace Celeste.Mod.StrawberryJam2021.Triggers {
     [CustomEntity("SJ2021/OshiroAttackTimeTrigger")]
     public class OshiroAttackTimeTrigger : Trigger {
         private bool Enable = true;
+        private static ILHook oshiroHook;
+        private static BindingFlags privateInstance = BindingFlags.NonPublic | BindingFlags.Instance;
+        private static MethodInfo oshiroCoroutineInfo = typeof(AngryOshiro).GetMethod("ChaseCoroutine", privateInstance).GetStateMachineTarget();
         public OshiroAttackTimeTrigger(EntityData data, Vector2 offset) : base(data, offset) {
             Enable = data.Bool("Enable", true);
         }
         public static void Load(){
-            IL.Celeste.AngryOshiro.ChaseCoroutine += ModAttackTime;
+            oshiroHook = new ILHook(oshiroCoroutineInfo, ModAttackTime);
         }
         public static void Unload() {
-            IL.Celeste.AngryOshiro.ChaseCoroutine -= ModAttackTime;
+            oshiroHook?.Dispose();
         }
 
         private static void ModAttackTime(ILContext il) {
             ILCursor cursor = new ILCursor(il);
-                while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdfld<AreaKey>("Mode"))) {
-                    Logger.Log("SJ2021", $"Modifying B-Side requirement for Oshiro timing @{cursor.Index}.");
-                    cursor.Emit(OpCodes.Pop);
-                    cursor.EmitDelegate<Func<bool, bool>>(val => StrawberryJam2021Module.Session.OshiroBSideMode || (Monocle.Engine.Scene as Level).Session.Area.Mode!=0);
-                    break;
-                }
+            FieldInfo f_this = oshiroCoroutineInfo.DeclaringType.GetField("<>4__this");
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdfld<AreaKey>("Mode"))) {
+                Logger.Log("SJ2021", $"Modifying B-Side requirement for Oshiro timing @{cursor.Index}.");
+                cursor.EmitDelegate<Func<int, int>>(val => (StrawberryJam2021Module.Session.OshiroBSideMode || (Monocle.Engine.Scene as Level).Session.Area.Mode!=0)?1:0);
+                break;
+            }
         }
 
         public override void OnEnter(Player player) {
