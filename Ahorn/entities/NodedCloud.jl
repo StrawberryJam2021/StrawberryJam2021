f9376be53fa6578dc294e94f99dbf15c352d2e48
Lollyde <github@lollyde.online>
2021-07-19 22:52:24 +0200

    noded cloud first implementation

---
+++ Ahorn/entities/NodedCloud.jl
@@ -0,0 +1,62 @@
+﻿module SJ2021NodedCloud
+
+using ..Ahorn, Maple
+
+@mapdef Entity "SJ2021/NodedCloud" NodedCloud(x::Integer, y::Integer, moveTime::Number=0.5,
+nodes::Array{Tuple{Integer, Integer}, 1}=Tuple{Integer, Integer}[])
+
+
+const placements = Ahorn.PlacementDict(
+    "Noded Cloud (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
+        NodedCloud
+    )
+)
+
+Ahorn.nodeLimits(entity::NodedCloud) = 0,-1
+
+sprite = "objects/clouds/fragile00"
+
+function renderNodedCloud(ctx::Ahorn.Cairo.CairoContext, x::Int, y::Int, alpha::Number=1.0)
+    Ahorn.drawImage(ctx, sprite, x - 18, y - 6; alpha=alpha) # dont ask me where those numbers come from, thats the offset needed to make the sprites line up properly
+end
+
+
+function Ahorn.selection(entity::NodedCloud)
+    nodes = get(entity.data, "nodes", ())
+
+    x, y = Ahorn.position(entity)
+
+    rect = Ahorn.Rectangle[Ahorn.getSpriteRectangle(sprite, x, y)]
+
+    for node in nodes
+        nx, ny = Int.(node)
+        push!(rect, Ahorn.getSpriteRectangle(sprite, nx, ny))
+    end
+    return rect
+end
+
+function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::NodedCloud, room::Maple.Room)
+    x,y = Ahorn.position(entity)
+    Ahorn.drawSprite(ctx, sprite, x, y)
+
+    for node in get(entity.data, "nodes", ())
+        nx, ny = Int.(node)
+        renderNodedCloud(ctx, nx, ny, 0.5)
+    end
+end
+
+function Ahorn.renderSelectedAbs(ctx::Ahorn.Cairo.CairoContext, entity::NodedCloud, room::Maple.Room)
+    x, y = Ahorn.position(entity)
+    oldx, oldy = x, y
+
+    #renderNodedCloud(ctx, x, y)
+
+    for node in get(entity.data, "nodes", ())
+        nx, ny = Int.(node)
+        Ahorn.drawArrow(ctx, oldx, oldy, nx, ny, Ahorn.colors.selection_selected_fc, headLength = 6)
+        renderNodedCloud(ctx, nx, ny, 0.5)
+        oldx, oldy = nx, ny
+    end
+end
+
+end
---
+++ Entities/NodedCloud.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using MonoMod.Utils;
+
+namespace Celeste.Mod.StrawberryJam2021.Entities {
+    [CustomEntity("SJ2021/NodedCloud")]
+    class NodedCloud : Cloud {
+        private readonly Vector2[] nodes;
+        private readonly DynData<Cloud> base_Entity;
+        private readonly Vector2 RoomOffset;
+        private Image outline;
+
+        private int nextNode = 0;
+        private float moveTime;
+
+        private bool fragile { set => base_Entity.Set("fragile", value); }
+        private float startY { set => base_Entity.Set("startY", value); }
+        private bool returning { get => base_Entity.Get<bool>("returning"); }
+        private Sprite sprite { get => base_Entity.Get<Sprite>("sprite"); }
+
+        public NodedCloud(EntityData data, Vector2 offset) : base(data.Position + offset, true) {
+            RoomOffset = offset;
+            moveTime = data.Float("moveTime", 0.5f);
+            nodes = data.Nodes;
+
+            base_Entity = new DynData<Cloud>(this);
+            Add(outline = new Image(GFX.Game["objects/StrawberryJam2021/nodedCloud/outline"]));
+            outline.CenterOrigin();
+            Add(new Coroutine(moveRoutine()));
+        }
+
+        public override void Added(Scene scene) {
+            base.Added(scene);
+            fragile = false;
+        }
+
+        public override void Render() {
+            if (nextNode  < nodes.Length) {
+                outline.RenderPosition = nodes[nextNode] + RoomOffset + sprite.Position;
+            } else {
+                outline.Visible = false;
+            }
+            base.Render();
+        }
+
+        private IEnumerator moveRoutine() {
+            while (nextNode + 1 <= nodes.Length) {
+                while (!returning) {
+                    yield return null;
+                }
+
+                float progress = 0f;
+                Vector2 oldPos = Position;
+                startY = (nodes[nextNode] + RoomOffset).Y;
+
+                while (progress < moveTime) {
+                    MoveTo(Vector2.Lerp(oldPos, nodes[nextNode] + RoomOffset, Ease.CubeIn(progress * (1 / moveTime))));
+                    progress += Engine.DeltaTime;
+                    yield return null;
+                }
+                Position = nodes[nextNode] + RoomOffset;
+                if (++nextNode == nodes.Length) {
+                    fragile = true;
+                }
+
+                while (returning) {
+                    yield return null;
+                }
+            }
+            while (Collidable) {
+                yield return null;
+            }
+            yield return 1f;
+            RemoveSelf();
+        }
+    }
+}
