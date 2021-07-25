ebc0e895f507edff7bd05cc0b4fc5c9f287da2aa
Shane Woolcock <samahnub@gmail.com>
2021-07-25 13:06:53 +0930

    Update Ahorn plugin

--- Ahorn/entities/paintbrush.jl
+++ Ahorn/entities/paintbrush.jl
@@ -5,9 +5,6 @@ using ..Ahorn, Maple
 const default_size = 16
 const thickness = 8
 
-const BEAM_THICKNESS = 12
-const BRUSH_LENGTH = 16
-
 @mapdef Entity "SJ2021/PaintbrushUp" PaintbrushUp(
     x::Integer, y::Integer, width::Integer=default_size,
     cassetteIndex::Integer=0, killPlayer::Bool=true, collideWithSolids::Bool=true, halfLength::Bool=false
@@ -34,7 +31,36 @@ const directions = Dict{String, String}(
     "SJ2021/PaintbrushLeft" => "left",
     "SJ2021/PaintbrushRight" => "right",
 )
-const brushLaserUnion = Union{PaintbrushUp, PaintbrushDown, PaintbrushLeft, PaintbrushRight}
+
+const scales = Dict{String, Tuple{Integer, Integer}}(
+    "up" => (1, -1),
+    "down" => (1, 1),
+    "left" => (-1, 1),
+    "right" => (1, 1),
+)
+
+const rotations = Dict{String, Number}(
+    "up" => 0,
+    "down" => pi,
+    "left" => -pi / 2,
+    "right" => pi / 2,
+)
+
+const smallOffsets = Dict{String, Tuple{Integer, Integer}}(
+    "up" => (12, -8),
+    "down" => (12, 8),
+    "left" => (-8, 4),
+    "right" => (8, 4),
+)
+
+const largeOffsets = Dict{String, Tuple{Integer, Integer}}(
+    "up" => (28, -24),
+    "down" => (28, 24),
+    "left" => (-16, 12),
+    "right" => (16, 12),
+)
+
+const paintbrushUnion = Union{PaintbrushUp, PaintbrushDown, PaintbrushLeft, PaintbrushRight}
 
 const placements = Ahorn.PlacementDict(
     "Paintbrush (Up) (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
@@ -88,18 +114,53 @@ Ahorn.minimumSize(entity::PaintbrushDown) = default_size, thickness
 Ahorn.minimumSize(entity::PaintbrushLeft) = thickness, default_size
 Ahorn.minimumSize(entity::PaintbrushRight) = thickness, default_size
 
-sprite_path = "objects/StrawberryJam2021/Paintbrush"
+sprite_path = "objects/StrawberryJam2021/paintbrush"
 
-function spriteForTexture(entity::brushLaserUnion)
+function largeSpriteForTexture(entity::paintbrushUnion)
     index = get(entity.data, "cassetteIndex", 0)
     prefix = index == 0 ? "blue" : "pink"
     return "$(sprite_path)/$(prefix)/brush1"
 end
 
-Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::PaintbrushUp, room::Maple.Room) = Ahorn.drawSprite(ctx, spriteForTexture(entity), -12, 0, jx=0, jy=0, rot=-pi/2)
-Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::PaintbrushDown, room::Maple.Room) = Ahorn.drawSprite(ctx, spriteForTexture(entity), -12, 0, sy=-1, jx=0, jy=0, rot=-pi/2)
+function smallSpriteForTexture(entity::paintbrushUnion)
+    index = get(entity.data, "cassetteIndex", 0)
+    prefix = index == 0 ? "blue" : "pink"
+    return "$(sprite_path)/$(prefix)/backbrush1"
+end
 
-Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::PaintbrushLeft, room::Maple.Room) = Ahorn.drawSprite(ctx, spriteForTexture(entity), 0, 0, sx=-1, jx=0, jy=0.5, rot=0)
-Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::PaintbrushRight, room::Maple.Room) = Ahorn.drawSprite(ctx, spriteForTexture(entity), 0, 0, jx=0, jy=0.5, rot=0)
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::paintbrushUnion, room::Maple.Room)
+    largeSprite = largeSpriteForTexture(entity)
+    smallSprite = smallSpriteForTexture(entity)
+    direction = get(directions, entity.name, "up")
+    width = get(entity.data, "width", 0)
+    height = get(entity.data, "height", 0)
+    vertical = direction == "up" || direction == "down"
+    size = vertical ? width : height
+    tiles = Integer(size / 8)
+    theta = vertical ? pi / 2 : 0
+    sx, sy = scales[direction]
+    sox, soy = smallOffsets[direction]
+    lox, loy = largeOffsets[direction]
+
+    # draw small
+    for num in 2:(tiles - 1)
+        if num % 2 != 0
+            continue
+        end
+        drawX = vertical ? num * 8 : 0
+        drawY = vertical ? 0 : num * 8
+        Ahorn.drawSprite(ctx, smallSprite, drawX + sox, drawY + soy, jx=0.5, jy=1, sx=sx, sy=sy, rot=theta)
+    end
+
+    # draw large
+    for num in 1:(tiles - 1)
+        if num % 2 == 0
+            continue
+        end
+        drawX = vertical ? num * 8 : 0
+        drawY = vertical ? 0 : num * 8
+        Ahorn.drawSprite(ctx, largeSprite, drawX + lox, drawY + loy, jx=0.5, jy=1, sx=sx, sy=sy, rot=theta)
+    end
+end
 
 end
