d83d0aa42b251d26c9600f39c9a64db7fd581307
JaThePlayer <janmalenta@gmail.com>
2021-08-01 22:21:18 +0200

    Fix the ahorn plugin

--- Ahorn/entities/constantDelayFallingBlockController.jl
+++ Ahorn/entities/constantDelayFallingBlockController.jl
@@ -2,24 +2,21 @@ module SJ2021ConstantDelayFallingBlockController
 
 using ..Ahorn, Maple
 
-@mapdef Entity "SJ2021/ConstantDelayFallingBlockController" ConstantDelayFallingBlock(x::Integer, y::Integer)
+@mapdef Entity "SJ2021/ConstantDelayFallingBlockController" ConstantDelayFallingBlockController(x::Integer, y::Integer)
 
 const placements = Ahorn.PlacementDict(
     "Constant Delay Falling Block Controller (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
-        ConstantDelayFallingBlock,
-        "rectangle",
-        Dict{String, Any}(),
-        Ahorn.tileEntityFinalizer
+        ConstantDelayFallingBlockController
     ),
 )
 
-function Ahorn.selection(entity::ConstantDelayFallingBlock)
+function Ahorn.selection(entity::ConstantDelayFallingBlockController)
     x, y = Ahorn.position(entity)
 
     return Ahorn.getSpriteRectangle("objects/StrawberryJam2021/constantDelayFallingBlockController", x, y)
 end
 
-function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::ConstantDelayFallingBlock)
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::ConstantDelayFallingBlockController)
     Ahorn.drawSprite(ctx, "objects/StrawberryJam2021/constantDelayFallingBlockController", 0, 0)
 end
 
