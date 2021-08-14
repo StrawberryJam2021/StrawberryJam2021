89539c7859f28df3084193980021061990252d26
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-08-14 17:34:56 +0200

    Set a parameter when condition is met I have no idea what I am doing just yet.

--- Entities/AirTimeMusicController.cs
+++ Entities/AirTimeMusicController.cs
@@ -1,5 +1,6 @@
 using Celeste;
 using Celeste.Mod.Entities;
+using FMOD.Studio;
 using Microsoft.Xna.Framework;
 using Monocle;
 
@@ -10,22 +11,29 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
     public class AirTimeMusicController : Entity {
         float airtimeThreshold = 0;
 
+        private EventInstance music;
+
         public AirTimeMusicController(EntityData data, Vector2 offset) : base(data.Position + offset) {
             airtimeThreshold = data.Float("activationThreshold");
+            param = data.Attr("longAirtimeParam", "long_airtime");
         }
 
         float lastGroundTime = 0;
 
+        string param;
+
         public override void Update() {
             base.Update();
+            music = Audio.CurrentMusicEventInstance;
+
             Player player = Scene.Tracker.GetEntity<Player>();
             if (player.OnSafeGround)
                 lastGroundTime = Scene.TimeActive;
 
             if (Scene.TimeActive - lastGroundTime > airtimeThreshold) {
-                // turn on music parameter
+                music.setParameterValue(param, 1);
             } else {
-                // turn off music parameter
+                music.setParameterValue(param, 0);
             }
         }
     }
