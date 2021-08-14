6559ca3ab88c761c72dacfbf9da82a6f121bc997
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-08-14 17:16:28 +0200

    Add to proper namespace

--- Entities/AirTimeMusicController.cs
+++ Entities/AirTimeMusicController.cs
@@ -1,26 +1,30 @@
 using Celeste;
+using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
 using Monocle;
 
-public class AirTimeMusicController : Entity {
-    float airtimeThreshold = 0;
+namespace Celeste.Mod.StrawberryJam2021.Entities {
+    [CustomEntity("SJ2021/AirTimeMusicController")]
+    public class AirTimeMusicController : Entity {
+        float airtimeThreshold = 0;
 
-    public AirTimeMusicController(EntityData data, Vector2 offset) : base(data.Position + offset) {
-        airtimeThreshold = data.Float("activationThreshold");
-    }
+        public AirTimeMusicController(EntityData data, Vector2 offset) : base(data.Position + offset) {
+            airtimeThreshold = data.Float("activationThreshold");
+        }
 
-    float lastGroundTime = 0;
+        float lastGroundTime = 0;
 
-    public override void Update() {
-        base.Update();
-        Player player = Scene.Tracker.GetEntity<Player>();
-        if (player.OnSafeGround)
-            lastGroundTime = Scene.TimeActive;
+        public override void Update() {
+            base.Update();
+            Player player = Scene.Tracker.GetEntity<Player>();
+            if (player.OnSafeGround)
+                lastGroundTime = Scene.TimeActive;
 
-        if (Scene.TimeActive - lastGroundTime > airtimeThreshold) {
-            // turn on music parameter
-        } else {
-            // turn off music parameter
+            if (Scene.TimeActive - lastGroundTime > airtimeThreshold) {
+                // turn on music parameter
+            } else {
+                // turn off music parameter
+            }
         }
     }
 }
