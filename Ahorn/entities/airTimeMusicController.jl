74a2c43ec78b4afe8ad119d36b734c68b2ef5e18
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-08-15 09:54:40 +0200

    Add ahorn entry

---
+++ Ahorn/entities/airTimeMusicController.jl
@@ -0,0 +1,18 @@
+module SJ2021
+
+using ..Ahorn, Maple
+
+@mapdef Entity "SJ2021/AirTimeMusicController" AirTimeMusicController(x::Integer, y::Integer, longAirtimeParam="long_airtime")
+
+const placements = Ahorn.PlacementDict(
+    "Airtime Music Controller (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
+        AirTimeMusicController
+    )
+)
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::AirTimeMusicController, room::Maple.Room)
+    Ahorn.drawRectangle(ctx, 0, 0, 8, 8, Ahorn.defaultWhiteColor, Ahorn.defaultBlackColor)
+end
+
+
+end
