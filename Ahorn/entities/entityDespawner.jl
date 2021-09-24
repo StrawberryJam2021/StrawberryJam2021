4109f729df7f7af658b760acdd7a148edef6dc87
tkavner <ominousfire@gmail.com>
2021-09-24 02:25:11 -0400

    Added preliminary Juila code for EntityDespawner

---
+++ Ahorn/entities/entityDespawner.jl
@@ -0,0 +1,19 @@
+module SJ2021
+
+using ..Ahorn, Maple
+
+@mapdef Entity "SJ2021/EntityDespawner" EntityDespawner(x::Integer, y::Integer, Name::String="",
+   NamesOfEntitiesToDespawn::String="", Despawn::Bool=true)
+
+const placements = Ahorn.PlacementDict(
+    "Entity Despawner (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
+        EntityDespawner
+    )
+)
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::EntityDespawner, room::Maple.Room)
+    Ahorn.drawRectangle(ctx, 0, 0, 8, 8, Ahorn.defaultWhiteColor, Ahorn.defaultBlackColor)
+end
+
+
+end
--- Entities/EntityDespawner.cs
+++ Entities/EntityDespawner.cs
@@ -10,42 +10,37 @@ namespace Celeste.Mod.StrawberryJam2021.Entities
     public class EntityDespawner : Entity
 	{
 		//fields
-		public bool Respawn; //inverts the thing
+		public bool Despawn; //inverts the thing
         public string NamesOfEntitiesToDespawn;
         public string FlagName;
         //constructors
         public EntityDespawner(EntityData data, Vector2 offset)
             : base(data.Position + offset) {
             NamesOfEntitiesToDespawn = data.Attr("NamesOfEntitiesToDespawn"); //think of a better name for this field, probably
-            Respawn = data.Attr("Respawn") == "true";
+            Despawn = data.Bool("Despawn");
             FlagName = data.Attr("Name");
         }
         //methods
 
         public override void Awake(Scene scene)
         {
-            List<Entity> entities = scene.Tracker.GetEntities<Entity>();
-            string[] entitiesToDespawn = NamesOfEntitiesToDespawn.Split(';');
-            foreach(Entity e in entities) {
-                CustomEntityAttribute entityAttribute =
-                    (CustomEntityAttribute) Attribute.GetCustomAttribute(e.GetType(), typeof(CustomEntityAttribute));
-                if (entityAttribute is null) continue;
-                else {
-                    foreach (string entityName in entityAttribute.IDs) {
-                        foreach (string name in entitiesToDespawn) {
-                            if (name.Equals(entityName)) {
-                                if (Respawn) {
-                                    //I feel like we should be checking if its already despawned / spawned from another controller maybe? not sure what field / attribute to check for that
-                                    //especially for the reverse?
-                                    //
-                                    //todo FIGURE OUT WHAT ADDS AN ENTITY BACK
-                                } else {
+            if (Despawn) {
+                List<Entity> entities = scene.Tracker.GetEntities<Entity>();
+                string[] entitiesToDespawn = NamesOfEntitiesToDespawn.Split(';');
+                foreach (Entity e in entities) {
+                    CustomEntityAttribute entityAttribute =
+                        (CustomEntityAttribute) Attribute.GetCustomAttribute(e.GetType(), typeof(CustomEntityAttribute));
+                    if (entityAttribute is null)
+                        continue;
+                    else {
+                        foreach (string entityName in entityAttribute.IDs) {
+                            foreach (string name in entitiesToDespawn) {
+                                if (name.Equals(entityName)) {
                                     e.RemoveSelf();
                                 }
                             }
                         }
                     }
-                    
                 }
             }
         }
