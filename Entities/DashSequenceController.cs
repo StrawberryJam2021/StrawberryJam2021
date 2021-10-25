3c269689fe5ff98e6fb68fd1c25628de1079d6fc
catapillie <espritox@gmail.com>
2021-10-25 17:32:10 +0200

    Finish DashSequenceDisplay behavior

--- Entities/DashSequenceController.cs
+++ Entities/DashSequenceController.cs
@@ -1,21 +1,36 @@
 ﻿using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
 using Monocle;
+using System.Linq;
 
 namespace Celeste.Mod.StrawberryJam2021.Entities {
     [CustomEntity("SJ2021/DashSequenceController")]
     [Tracked]
     public class DashSequenceController : Entity {
-        public string[] DashCode;
+        public Vector2[] DashCode;
         public string FlagLabel;
         public string FailureFlag;
         public int Index;
 
         private int codePosition;
 
+        private DashSequenceDisplay display;
+
         public DashSequenceController(EntityData data, Vector2 offset) 
             : base(data.Position + offset) {
-            DashCode = data.Attr("dashCode", "*").ToUpper().Split(',');
+            DashCode = data.Attr("dashCode", "*").ToUpper()
+                .Split(',').Select(s => s switch {
+                    "U" => -Vector2.UnitY,
+                    "D" => Vector2.UnitY,
+                    "L" => -Vector2.UnitX,
+                    "R" => Vector2.UnitX,
+                    "UL" => new Vector2(-1, -1),
+                    "UR" => new Vector2(1, -1),
+                    "DL" => new Vector2(-1, 1),
+                    "DR" => new Vector2(1, 1),
+                    _ => Vector2.Zero,
+                }).ToArray();
+
             FlagLabel = data.Attr("flagLabel", "");
             FailureFlag = data.Attr("flagOnFailure", "");
             Index = data.Int("index");
@@ -40,40 +55,33 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
             codePosition = 0;
         }
 
-        public override void Awake(Scene scene) {
-            base.Awake(scene);
-            if (scene.Tracker.GetEntity<DashSequenceDisplay>() == null)
-                scene.Add(new DashSequenceDisplay());
+
+        public override void Update() {
+            base.Update();
+            if (display == null)
+                display = Scene.Tracker.GetEntity<DashSequenceDisplay>();
         }
 
         private void OnDash(Vector2 direction) {
-            string input = "";
-
-            if (direction.Y < 0f) {
-                input = "U";
-            } else if (direction.Y > 0f) {
-                input = "D";
-            }
-            if (direction.X < 0f) {
-                input += "L";
-            } else if (direction.X > 0f) {
-                input += "R";
-            }
-
-            if (input == DashCode[codePosition] || DashCode[codePosition] == "*") {
-                SceneAs<Level>().Session.SetFlag(FlagLabel + "-" + (codePosition + 1), true);
-                codePosition++;
-                if (codePosition >= DashCode.Length) {
-                    RemoveSelf();
+            if (display != null && Index == display.Index) {
+                Vector2 dir = Calc.Sign(direction);
+                if (DashCode[codePosition] == Vector2.Zero || dir == DashCode[codePosition]) {
+                    SceneAs<Level>().Session.SetFlag(FlagLabel + "-" + (codePosition + 1), true);
+                    codePosition++;
+                    display.ValidateInput();
+                    if (codePosition >= DashCode.Length) {
+                        RemoveSelf();
+                    }
+                } else {
+                    for (int i = 1; i <= DashCode.Length; i++) {
+                        SceneAs<Level>().Session.SetFlag(FlagLabel + "-" + i, false);
+                    }
+                    if (codePosition != 0 && !string.IsNullOrEmpty(FailureFlag)) {
+                        SceneAs<Level>().Session.SetFlag(FailureFlag, true);
+                    }
+                    codePosition = 0;
+                    display.Fail();
                 }
-            } else {
-                for (int i = 1; i <= DashCode.Length; i++) {
-                    SceneAs<Level>().Session.SetFlag(FlagLabel + "-" + i, false);
-                }
-                if (codePosition != 0 && !string.IsNullOrEmpty(FailureFlag)) {
-                    SceneAs<Level>().Session.SetFlag(FailureFlag, true);
-                }
-                codePosition = 0;
             }
         }
     }
--- Entities/DashSequenceDisplay.cs
+++ Entities/DashSequenceDisplay.cs
@@ -8,22 +8,30 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
     // This entity is added/removed from the scene automatically.
     [Tracked]
     public class DashSequenceDisplay : Entity {
+        private static readonly Color correctA = Color.LimeGreen;
+        private static readonly Color correctB = Calc.HexToColor("1bd1bc"); // cyan
+
         private float drawlerp;
         private readonly MTexture bg = GFX.Gui["strawberryCountBG"];
-        private readonly MTexture arrow = GFX.Gui["dotarrow_outline"];
 
-        private readonly SortedList<int, string[]> dashCodes = new SortedList<int, string[]>();
+        private readonly SortedList<int, MTexture[]> dashCodes = new SortedList<int, MTexture[]>();
+
+        public int Index;
+        private float lengthTarget, lengthPrev, lengthLerp;
+        private MTexture[] currentCodeArrows;
+        private KeyValuePair<int, MTexture[]>? nextCode;
+        private float[] currentCodeArrowsAnim;
+        private float animThreshold;
+
+        private int codePosition;
+        private readonly Wiggler wiggler;
 
         public DashSequenceDisplay() {
             Tag = Tags.HUD | Tags.Global | Tags.PauseUpdate | Tags.TransitionUpdate;
             Depth = -100;
             Y = 96f;
-        }
-
-        public void AddDashCode(int index, string[] dashCode) {
-            System.Console.WriteLine(index + " " + dashCode.Aggregate((a, b) => $"{a},{b}"));
-            if (!dashCodes.ContainsKey(index))
-                dashCodes.Add(index, dashCode);
+            Add(wiggler = Wiggler.Create(0.5f, 2f));
+            Add(new Coroutine(ChangingDashCodeRoutine()));
         }
 
         public override void Update() {
@@ -39,30 +47,123 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
             Y = Calc.Approach(Y, y, Engine.DeltaTime * 800f);
 
             drawlerp = Calc.Approach(drawlerp, true ? 1 : 0, Engine.DeltaTime * 2);
+            lengthLerp = Calc.Approach(lengthLerp, 1, Engine.DeltaTime * 1.5f);
+
+            if (currentCodeArrowsAnim != null)
+                for (int i = 0; i < currentCodeArrowsAnim.Length; i++)
+                    currentCodeArrowsAnim[i] = Calc.Approach(currentCodeArrowsAnim[i], i < animThreshold * currentCodeArrowsAnim.Length ? 1f : 0f, Engine.DeltaTime * 2.5f);
+
             base.Update();
         }
 
-        public void InitializeDashCodes() {
+        public void InitializeDashCodes(Level level) {
             dashCodes.Clear();
-            foreach (DashSequenceController controller in Scene.Tracker.GetEntities<DashSequenceController>()) {
-                dashCodes[controller.Index] = controller.DashCode;
+            DashSequenceController[] controllers = level.Tracker.GetEntities<DashSequenceController>().Cast<DashSequenceController>().ToArray();
+            if (controllers.Length != 0) {
+                foreach (DashSequenceController controller in controllers)
+                    dashCodes[controller.Index] = controller.DashCode.Select(v => GFX.Gui[$"controls/directions/{(int) v.X}x{(int) v.Y}"]).ToArray();
+
+                if (dashCodes.Count != 0)
+                    nextCode = dashCodes.First();
+            } else
+                nextCode = null;
+        }
+
+        public void ValidateInput() {
+            codePosition++;
+            wiggler.Start();
+            if (codePosition >= currentCodeArrows.Length) {
+                KeyValuePair<int, MTexture[]> next = dashCodes.FirstOrDefault(pair => pair.Key > Index);
+                if (next.Value != null)
+                    nextCode = next;
+                else
+                    nextCode = null;
             }
-            System.Console.WriteLine(dashCodes.Count);
+        }
+
+        public void Fail() {
+            codePosition = 0;
         }
 
         public override void Awake(Scene scene) {
             base.Awake(scene);
-            InitializeDashCodes();
+            InitializeDashCodes(SceneAs<Level>());
         }
 
         public override void Render() {
             if (!(drawlerp <= 0f)) {
                 Vector2 pos = Vector2.Lerp(new Vector2(-bg.Width, Y), Vector2.UnitY * Y, Ease.CubeOut(drawlerp));
                 pos = pos.Round();
-                bg.DrawJustified(pos, new Vector2(0f, 0.5f), Color.White, Vector2.One * 1.25f);
 
-                Vector2 arrowOffset = pos + Vector2.UnitX * (arrow.Width / 2);
-                arrow.DrawCentered(arrowOffset);
+                float l = lengthPrev + (lengthTarget - lengthPrev) * Ease.CubeOut(lengthLerp);
+                Vector2 bgPos = pos + Vector2.UnitX * (32 + (l - 7.5f) * 48);
+                if (bgPos.X > 0f)
+                    bg.DrawJustified(pos, new Vector2(0f, 0.5f), Color.White, new Vector2((bgPos.X - pos.X + 64) / bg.Width, 1.25f));
+                bg.DrawJustified(bgPos, new Vector2(0f, 0.5f), Color.White, Vector2.One * 1.25f);
+
+                if (currentCodeArrows != null) {
+                    Vector2 arrowOffset = pos + Vector2.UnitX * 32;
+                    for (int i = 0; i < currentCodeArrows.Length; i++) {
+                        Color c = Color.White;
+                        float s = Ease.ExpoInOut(currentCodeArrowsAnim[i]) * 0.75f, r = 0;
+                        if (i < codePosition) {
+
+                            c = Settings.Instance.DisableFlashes ? correctA : Color.Lerp(correctA, correctB, Calc.SineMap(Scene.RawTimeActive * 20f - i / 2f, 0f, 1f));
+                        }
+                        if (i + 1 == codePosition) {
+                            s += wiggler.Value * 0.25f;
+                            r = wiggler.Value * 0.25f;
+                        }
+                        currentCodeArrows[i].DrawCentered(arrowOffset + Vector2.UnitX * (i * 48), c, s, r);
+                    }
+                }
+            }
+        }
+
+        private IEnumerator ChangingDashCodeRoutine() {
+            while (true) {
+                while (this.nextCode?.Value == currentCodeArrows)
+                    yield return null;
+
+                KeyValuePair<int, MTexture[]> nextCode = this.nextCode ?? default;
+
+                int count = -1;
+                if (nextCode.Value != null)
+                    count = nextCode.Value.Length;
+                else if (currentCodeArrows != null)
+                    yield return 0.5f;
+
+                bool changedLength = false;
+                if (currentCodeArrows != null) {
+                    while (animThreshold > 0f) {
+                        animThreshold = Calc.Approach(animThreshold, 0f, Engine.DeltaTime * 4f);
+                        yield return null;
+                    }
+
+                    lengthPrev = lengthTarget;
+                    lengthLerp = 0f;
+                    lengthTarget = count;
+                    changedLength = true;
+                    yield return nextCode.Value == null ? 1f : 0.3f;
+                }
+
+                if (nextCode.Value != null) {
+                    if (!changedLength) {
+                        lengthPrev = lengthTarget;
+                        lengthLerp = 0f;
+                        lengthTarget = count;
+                    }
+                    Index = nextCode.Key;
+                    currentCodeArrows = nextCode.Value;
+                    currentCodeArrowsAnim = new float[count];
+                    codePosition = 0;
+
+                    animThreshold = 0f;
+                    while (animThreshold < 1f) {
+                        animThreshold = Calc.Approach(animThreshold, 1f, Engine.DeltaTime * 4f);
+                        yield return null;
+                    }
+                }
             }
         }
 
@@ -70,10 +171,12 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
 
         internal static void Load() {
             On.Celeste.Level.TransitionRoutine += Level_TransitionRoutine;
+            On.Celeste.LevelLoader.LoadingThread += LevelLoader_LoadingThread;
         }
 
         internal static void Unload() {
             On.Celeste.Level.TransitionRoutine -= Level_TransitionRoutine;
+            On.Celeste.LevelLoader.LoadingThread -= LevelLoader_LoadingThread;
         }
 
         private static IEnumerator Level_TransitionRoutine(On.Celeste.Level.orig_TransitionRoutine orig, Level self, LevelData next, Vector2 direction) {
@@ -81,7 +184,13 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
 
             DashSequenceDisplay display = self.Tracker.GetEntity<DashSequenceDisplay>();
             if (display != null)
-                display.InitializeDashCodes();
+                display.InitializeDashCodes(self);
+        }
+
+        private static void LevelLoader_LoadingThread(On.Celeste.LevelLoader.orig_LoadingThread orig, LevelLoader self) {
+            orig(self);
+            if (self.Level.Tracker.GetEntity<DashSequenceDisplay>() == null)
+                self.Level.Add(new DashSequenceDisplay());
         }
 
         #endregion
