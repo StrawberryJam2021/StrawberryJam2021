9cb85904cbe50995d51ca34ba6c96817bb602784
Viv-0 <reserve.power.2514.2@gmail.com>
2021-11-26 19:19:34 -0500

    Resolved Spacing in julia files

--- Ahorn/entities/darkMatter.jl
+++ Ahorn/entities/darkMatter.jl
@@ -1,5 +1,4 @@
 ﻿module SJ2021DarkMatter
-
 using ..Ahorn, Maple
 
 @mapdef Entity "SJ2021/DarkMatter" DarkMatter(x::Integer,y::Integer,width::Integer=8,height::Integer=8)
@@ -13,19 +12,13 @@ const placements = Ahorn.PlacementDict(
 
 Ahorn.minimumSize(entity::DarkMatter) = 8, 8
 Ahorn.resizable(entity::DarkMatter) = true, true
-
 Ahorn.selection(entity::DarkMatter) = Ahorn.getEntityRectangle(entity)
-
 
 function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::DarkMatter, room::Maple.Room)
     width = Int(get(entity.data, "width", 32))
     height = Int(get(entity.data, "height", 32))
-
-	realEdgeColor = (172.0,41.0,242.0, 255.0) ./ 255
-	
-
-	realCenterColor = (136.0,1.0,208.0,255.0) ./ 255
-
+    realEdgeColor = (172.0,41.0,242.0, 255.0) ./ 255
+    realCenterColor = (136.0,1.0,208.0,255.0) ./ 255
     Ahorn.drawRectangle(ctx, 0, 0, width, height, realCenterColor, realEdgeColor)
 end
 
