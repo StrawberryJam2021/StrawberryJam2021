4003a87fc219938524365dd3b784adb1adcd4d18
Jonathan Long <jonathan.luke.long@gmail.com>
2022-01-13 17:07:57 -0500

    Implement RequireDashlessTrigger
    Dead simple way to apply winged golden behavior to other entities. Provide a list of entity names to remove, then check collision with the trigger and remove any entities within it.

---
+++ Triggers/RequireDashlessTrigger.cs
@@ -0,0 +1,34 @@
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Linq;
+
+namespace Celeste.Mod.StrawberryJam2021.Triggers {
+    [CustomEntity("SJ2021/RequireDashlessTrigger")]
+    class RequireDashlessTrigger : Trigger {
+        private static char[] separators = { ',' };
+
+        public string[] EntityNames;
+
+        public RequireDashlessTrigger(EntityData data, Vector2 offset) : base(data, offset) {
+            EntityNames = data.Attr("entityNames", "").Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public override void Added(Scene scene) {
+            base.Added(scene);
+
+            Level level = SceneAs<Level>();
+
+            if (level.Session.Dashes != 0 || !level.Session.StartedFromBeginning) {
+                foreach (Entity entity in scene.Entities) {
+                    if (entity.CollideCheck(this)) {
+                        if (Enumerable.Contains(EntityNames, entity.GetType().FullName) || Enumerable.Contains(EntityNames, entity.GetType().Name)) {
+                            scene.Remove(entity);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
