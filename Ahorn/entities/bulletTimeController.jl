e0e3887ef5980ba13b1c0542406d5698fcbacdfe
Jonathan Long <jonathan.luke.long@gmail.com>
2022-04-26 18:45:42 -0400

    Adds flag toggle, fixes bug
    - Adds optional flag toggling
    - Fixes slowed pause menu (this bug occurs in vanilla Oshiro/seeker slowdown lol)
    - Only resets time rate once so we don't interfere with other entities e.g. heart collects

--- Ahorn/entities/bulletTimeController.jl
+++ Ahorn/entities/bulletTimeController.jl
@@ -2,13 +2,19 @@ module SJ2021BulletTimeController
 
 using ..Ahorn, Maple
 
-@mapdef Entity "SJ2021/BTController" BTController(x::Integer, y::Integer, speed::Number = 0.5)
+@mapdef Entity "SJ2021/BTController" BTController(x::Integer, y::Integer, speed::Number = 0.5, flag::String="")
 
 const placements = Ahorn.PlacementDict(
     "Bullet Time Controller (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
-        BTController
+        BTController,
+        "rectangle"
     )
 )
+
+function Ahorn.selection(entity::BTController)
+    x, y = Ahorn.position(entity)
+    return Ahorn.Rectangle(x, y, 8, 8)
+end
 
 function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::BTController, room::Maple.Room)
     Ahorn.drawRectangle(ctx, 0, 0, 8, 8, Ahorn.defaultWhiteColor, Ahorn.defaultBlackColor)
