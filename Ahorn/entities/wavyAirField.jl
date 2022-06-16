2e204d62025ab9d2253728270044bb1e49ede4b2
Jonathan Long <jonathan.luke.long@gmail.com>
2022-06-16 16:49:45 -0400

    Add WavyAirField
    Basically just a HeatWave distortion field w/o the rest of the effects, needed for Solar Express

---
+++ Ahorn/entities/wavyAirField.jl
@@ -0,0 +1,23 @@
+﻿module SJ2021WavyAirField
+using ..Ahorn, Maple
+
+@mapdef Entity "SJ2021/WavyAirField" WavyAirField(x::Integer,y::Integer,width::Integer=8,height::Integer=8)
+
+const placements = Ahorn.PlacementDict(
+    "Wavy Air Field (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
+        WavyAirField,
+        "rectangle"
+    )
+)
+
+Ahorn.minimumSize(entity::WavyAirField) = 8, 8
+Ahorn.resizable(entity::WavyAirField) = true, true
+Ahorn.selection(entity::WavyAirField) = Ahorn.getEntityRectangle(entity)
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::WavyAirField, room::Maple.Room)
+    width = Int(get(entity.data, "width", 8))
+    height = Int(get(entity.data, "height", 8))
+    Ahorn.drawRectangle(ctx, 0, 0, width, height, (0.3, 0.3, 1.0, 0.4), (0.3, 0.3, 1.0, 1.0))
+end
+
+end
