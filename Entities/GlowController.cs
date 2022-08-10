4309593a9fe83f263b21d9f00c1dce0917441ee4
Shane Woolcock <samahnub@gmail.com>
2022-08-10 21:15:20 +0930

    Fade out VertexLight and BloomPoint when gliders get vaporised

--- Entities/GlowController.cs
+++ Entities/GlowController.cs
@@ -1,6 +1,8 @@
 using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
 using Monocle;
+using System;
+using System.Collections;
 using System.Linq;
 
 namespace Celeste.Mod.StrawberryJam2021.Entities {
@@ -46,18 +48,48 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
             foreach (var entity in allEntities) {
                 var type = entity.GetType();
                 var typeName = type.FullName;
+                var requiresRemovalRoutine = false;
 
                 if (_lightWhitelist.Contains(typeName)) {
                     entity.Add(new VertexLight(_lightOffset, _lightColor, _lightAlpha, _lightStartFade, _lightEndFade));
+                    requiresRemovalRoutine = true;
                 } else if (_lightBlacklist.Contains(typeName)) {
                     entity.Remove(entity.Components.GetAll<VertexLight>().ToArray<Component>());
                 }
 
                 if (_bloomWhitelist.Contains(typeName)) {
                     entity.Add(new BloomPoint(_bloomOffset, _bloomAlpha, _bloomRadius));
+                    requiresRemovalRoutine = true;
                 } else if (_bloomBlacklist.Contains(typeName)) {
                     entity.Remove(entity.Components.GetAll<BloomPoint>().ToArray<Component>());
                     entity.Remove(entity.Components.GetAll<CustomBloom>().ToArray<Component>());
+                }
+
+                // gliders get a special coroutine that hides lights and blooms when they're vaporised
+                if (requiresRemovalRoutine && entity is Glider glider) {
+                    glider.Add(new Coroutine(GliderRoutine(glider)));
+                }
+            }
+        }
+
+        private IEnumerator GliderRoutine(Glider glider) {
+            var sprite = glider.Get<Sprite>();
+            while (glider.Scene != null && sprite.CurrentAnimationID != "death") {
+                yield return null;
+            }
+
+            const float fadeTime = 0.5f;
+            var fadeRemaining = fadeTime;
+            
+            while (fadeRemaining > 0) {
+                fadeRemaining -= Engine.DeltaTime;
+                var alpha = Math.Max(fadeRemaining / fadeTime, 0f);
+                
+                foreach (VertexLight vertexLight in glider.Components.GetAll<VertexLight>()) {
+                    vertexLight.Alpha = alpha;
+                }
+                foreach (BloomPoint bloomPoint in glider.Components.GetAll<BloomPoint>()) {
+                    bloomPoint.Alpha = alpha;
                 }
             }
         }
