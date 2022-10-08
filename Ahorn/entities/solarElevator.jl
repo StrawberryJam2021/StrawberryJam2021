4762abb78da424985543a4b572e5ca4473eced04
catapillie <espritox@gmail.com>
2022-10-08 16:21:10 +0200

    WIP Solar Elevator implementation

--- Ahorn/entities/solarElevator.jl
+++ Ahorn/entities/solarElevator.jl
@@ -2,7 +2,7 @@ module SJ2021SolarElevator
 
 using ..Ahorn, Maple
 
-@mapdef Entity "SJ2021/SolarElevator" SolarElevator(x::Integer, y::Integer, height::Integer=128)
+@mapdef Entity "SJ2021/SolarElevator" SolarElevator(x::Integer, y::Integer, distance::Integer=128, time::Number=3.0)
 
 const placements = Ahorn.PlacementDict(
     "Solar Elevator (Strawberry Jam 2021)" => Ahorn.EntityPlacement(
@@ -13,14 +13,11 @@ const placements = Ahorn.PlacementDict(
 
 function Ahorn.selection(entity::SolarElevator)
     x, y = Ahorn.position(entity)
-    return Ahorn.Rectangle(x - 24, y - 80, 48, 80)
+    return Ahorn.Rectangle(x - 24, y - 70, 48, 80)
 end
 
 function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::SolarElevator, room::Maple.Room)
-    sprite = Ahorn.getSprite("objects/StrawberryJam2021/solarElevator/elevatorback", "Gameplay")
-    Ahorn.drawImage(ctx, sprite, -floor(Int, sprite.width / 2), -sprite.height)
-    sprite = Ahorn.getSprite("objects/StrawberryJam2021/solarElevator/elevator", "Gameplay")
-    Ahorn.drawImage(ctx, sprite, -floor(Int, sprite.width / 2), -sprite.height)
+    Ahorn.drawImage(ctx, "objects/StrawberryJam2021/solarElevator/elevator", -28, -70)
 end
 
 end
---
+++ Entities/SolarElevator.cs
@@ -0,0 +1,75 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System.Collections;
+
+namespace Celeste.Mod.StrawberryJam2021.Entities {
+    [CustomEntity("SJ2021/SolarElevator")]
+    public class SolarElevator : Solid {
+        private readonly TalkComponent interaction;
+        private readonly SoundSource moveSfx;
+
+        private readonly int distance;
+        private readonly float time;
+
+        private bool enabled = false;
+        private bool atGroundFloor = true;
+
+        public SolarElevator(EntityData data, Vector2 offset)
+            : this(data.Position + offset, data.Int("distance", 128), data.Float("time", 3.0f)) { }
+
+        public SolarElevator(Vector2 position, int distance, float time)
+            : base(position, 56, 80, safe: true) {
+            Depth = Depths.Player - 1;
+            SurfaceSoundIndex = SurfaceIndex.MoonCafe;
+
+            this.distance = distance;
+            this.time = time;
+
+            Collider = new Hitbox(48, 5, -24, 0);
+
+            Add(moveSfx = new());
+
+            Add(interaction = new TalkComponent(new Rectangle(-12, -8, 24, 8), Vector2.UnitY * -16, Activate));
+
+            Image img = new(GFX.Game["objects/StrawberryJam2021/solarElevator/elevator"]);
+            img.JustifyOrigin(0.5f, 1.0f);
+            img.Position.Y = 10;
+            Add(img);
+        }
+
+        private void Activate(Player player) {
+            if (!enabled)
+                Add(new Coroutine(Sequence()));
+        }
+
+        private IEnumerator Sequence() {
+            enabled = true;
+            interaction.Enabled = false;
+            Audio.Play(SFX.game_10_ppt_mouseclick, Position);
+
+            yield return 1f;
+
+            moveSfx.Play(SFX.game_04_gondola_cliffmechanism_start);
+            SceneAs<Level>().DirectionalShake(Vector2.UnitY, 0.15f);
+
+            float start = Y;
+            float end = atGroundFloor ? (start - distance) : (start + distance);
+            float t = 0.0f;
+            while (t < time) {
+                float percent = t / time;
+                MoveToY(start + (end - start) * percent);
+
+                t = Calc.Approach(t, time, Engine.DeltaTime);
+                yield return null;
+            }
+
+            MoveToY(end);
+            moveSfx.Stop();
+
+            enabled = false;
+            interaction.Enabled = true;
+            atGroundFloor = !atGroundFloor;
+        }
+    }
+}
