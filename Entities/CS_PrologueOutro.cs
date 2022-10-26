3c11b13b9755bb5e4c8172d863597b9c2b69170e
amctyg <amctyg@umsystem.edu>
2022-10-25 22:54:39 -0500

    add graphics and basic entities

---
+++ Entities/CS_PrologueOutro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using Celeste.Mod.Entities;
+using Monocle;
+
+namespace Celeste.Mod.StrawberryJam2021.Entities {
+    [CustomEntity("SJ2021/CS_PrologueOutro")]
+    public class CS_PrologueOutro : CutsceneEntity {
+        private Player player;
+
+        public CS_PrologueOutro(Player player) {
+            this.player = player;
+        }
+
+        public override void OnBegin(Level level) {
+            Add(new Coroutine(Cutscene(level)));
+        }
+
+        private IEnumerator Cutscene(Level level) {
+            yield return 0.0f;
+            EndCutscene(level);
+        }
+
+        public override void OnEnd(Level level) {
+        }
+    }
+}
---
+++ Entities/PrologueBasket.cs
@@ -0,0 +1,33 @@
+﻿using Monocle;
+using System;
+using Celeste;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections;
+using Celeste.Mod.Entities;
+
+namespace Celeste.Mod.StrawberryJam2021.Entities {
+    [CustomEntity("SJ2021/PrologueBasket")]
+    public class PrologueBasket : Entity{
+        private TalkComponent talk;
+        private MTexture sprite;
+        private Vector2 position;
+
+        public PrologueBasket(EntityData data, Vector2 offset) : base(data.Position + offset) {
+            base.Depth = 2000;
+            Add(talk = new TalkComponent(new Rectangle(-24, -8, 48, 40), new Vector2(0f, 0f), Interact));
+            talk.PlayerMustBeFacing = false;
+            position = data.Position + offset;
+            sprite = GFX.Game["objects/StrawberryJam2021/prologueBasket/basket"];
+        }
+
+        public override void Render() {
+            base.Render();
+            sprite.DrawCentered(position);
+        }
+
+        private void Interact(Player player) {
+            Scene.Add(new CS_PrologueOutro(player));
+        }
+    }
+}
