b6e89a0ac448683aaa6492b8cb860930b6529de4
amctyg <amctyg@umsystem.edu>
2022-10-27 00:14:55 -0500

    title logo creation successful, add particles

--- Entities/CS_PrologueOutro.cs
+++ Entities/CS_PrologueOutro.cs
@@ -8,30 +8,100 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
     [CustomEntity("SJ2021/CS_PrologueOutro")]
     public class CS_PrologueOutro : CutsceneEntity {
 
+        public class TitleLogo : Entity {
+
+            private class Particle {
+                public readonly MTexture texture = OVR.Atlas["star"].GetSubtexture(1, 1, 256, 256);
+                public const float DefaultParticleLifetime = 3f;
+                public const float ParticleLifetimeVariance = 1f;
+                public Vector2 offset;
+                public float opacity;
+                public float size;
+
+                private float livedTime;
+                private float lifetime;
+
+                public Particle(float timeOffset) {
+                    Reset(timeOffset);
+                }
+
+                public void Update() {
+                    if (livedTime > lifetime) {
+                        Reset(0f, Calc.Random.NextFloat(ParticleLifetimeVariance) - (ParticleLifetimeVariance / 2) + DefaultParticleLifetime);
+                    }
+                    livedTime += Engine.DeltaTime;
+                    float percLived = livedTime / lifetime;
+                    float adjustedEaser = (-4 * (float)Math.Pow(percLived - 0.5f, 2)) + 1;
+                    opacity = adjustedEaser;
+                    size = (adjustedEaser / 2) + 0.5f;
+                }
+
+                private void Reset(float time = 0f, float lifetime = DefaultParticleLifetime) {
+                    this.lifetime = lifetime;
+                    livedTime = time % lifetime;
+                    offset = new Vector2(Calc.Random.Next(-180, 180), Calc.Random.Next(-90, 90));
+                    opacity = 0f;
+                    size = 0.5f;
+                }
+            }
+
+            private const int ParticleCount = 5;
+            private ArrayList particles;
+
+            private Sprite sprite;
+            private float opacity;
+            private float size;
+
+            public TitleLogo() {
+                sprite = new Sprite(GFX.Gui, "StrawberryJam2021/logo/logo");
+                sprite.Add("idle", "", 0.07f);
+                sprite.Play("idle");
+                Tag = Tags.HUD;
+                opacity = 0f;
+                size = 0f;
+                particles = new ArrayList();
+                for (int i = 0; i < ParticleCount; i++) {
+                    particles.Add(new Particle(Calc.Random.NextFloat(Particle.DefaultParticleLifetime)));
+                }
+            }
+
+            public override void Render() {
+                sprite.Texture.DrawCentered(Celeste.TargetCenter, Color.White * opacity, size);
+                foreach (Particle particle in particles) {
+                    particle.texture.DrawCentered(Celeste.TargetCenter + (particle.offset * size), Color.White * particle.opacity * opacity, particle.size * size * 0.25f);
+                }
+            }
+
+            public override void Update() {
+                base.Update();
+                foreach (Particle particle in particles) {
+                    particle.Update();
+                }
+            }
+
+            public IEnumerator EaseIn() {
+                for (float p = 0f; p < 1f; p += Engine.DeltaTime / 3) {
+                    opacity = Ease.CubeOut(p);
+                    size = (Ease.CubeOut(p) / 2) + 0.5f;
+                    yield return null;
+                }
+                opacity = 1f;
+                size = 1f;
+            }
+        }
+
         private Player player;
         private PrologueBasket basket;
-        private Sprite logo;
-        private float logoOpacity;
-        private float logoSize;
+        private TitleLogo logo;
         private int state; // TEMP, allows me to restart the cutscene when it finishes
 
         public CS_PrologueOutro(Player player, PrologueBasket basket) {
             this.player = player;
             this.basket = basket;
-            logo = new Sprite(GFX.Gui, "StrawberryJam2021/logo/logo");
-            logo.Add("idle", "", 0.07f);
-            logo.Play("idle");
-            logo.Visible = false;
-            logoOpacity = 1f; // TEMP set to 1f so that the sprite is at least visible
-            logoSize = 1f; // TEMP set to 1f so that the sprite is at least visible
         }
 
-        public override void Render() {
-            logo.Texture.DrawCentered(logo.Position, Color.White * logoOpacity, logoSize); 
-            // I'm copying from shatter cassette and this bullshit does not work
-            // I have NO idea why but this draws the sprite way too large and pixelated despite being taken from GUI Atlas
-            // If someone could please explain to me how to render digital art vs pixel art I would be so happy
-            // Also it would be nice if there was a function that draws relative to the screen not the level, once again, copying stuff from BreathingMinigame just didn't work
+        public override void Update() {
+            base.Update();
         }
 
         public override void OnBegin(Level level) {
@@ -51,23 +121,14 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
             player.Sprite.Play("sitDown");
             yield return 2f;
             yield return PanCamera(level);
-            logo.Position = level.Camera.Position; // If possible get rid of this line
-            logo.Visible = true;
-            LogoEaseIn(); // don't know if should be yield return, should people be able to confirm before the logo is fully formed?
+            logo = new TitleLogo();
+            Scene.Add(logo);
+            yield return logo.EaseIn(); // don't know if should be yield return, should people be able to confirm before the logo is fully formed?
             while (!Input.MenuConfirm.Pressed) {
                 yield return null;
             }
             logo.Visible = false;
             EndCutscene(level);
-        }
-
-        private IEnumerator LogoEaseIn() {
-            // This should just set an alpha value for the logo, and also increase the size, idk why but it's not working properly
-            for (float p = 0f; p < 1f; p += Engine.DeltaTime / 3) {
-                logoOpacity = Ease.CubeOut(p);
-                logoSize = (Ease.CubeOut(p) / 2) + 0.5f;
-                yield return null;
-            }
         }
 
         private IEnumerator PanCamera(Level level) {
