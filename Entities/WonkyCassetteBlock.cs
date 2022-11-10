333d01aeb77a2b3dcd06412a179dd30594382233
Vexatos <Stuarzt@gmx.de>
2022-11-10 14:47:16 +0100

    Fix cassette blocks in group having boost last one frame less

--- Entities/WonkyCassetteBlock.cs
+++ Entities/WonkyCassetteBlock.cs
@@ -20,6 +20,7 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
 
         private readonly int OverrideBoostFrames;
         public int boostFrames = 0;
+        public bool boostActive = false;
 
         private DynamicData cassetteBlockData;
 
@@ -97,11 +98,19 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
                         }
                     }
 
+                    boostActive = true;
+
                     var group = cassetteBlockData.Get<List<CassetteBlock>>("group");
                     foreach (CassetteBlock cassetteBlock in group) {
-                        ((WonkyCassetteBlock) cassetteBlock).boostFrames = boostFrames;
+                        WonkyCassetteBlock wonkyBlock = (WonkyCassetteBlock) cassetteBlock;
+                        wonkyBlock.boostFrames = boostFrames;
+                        wonkyBlock.boostActive = true;
                     }
+                }
 
+                if (boostActive) {
+                    // Vanilla lift boost is active this frame, do nothing
+                    boostActive = false;
                 } else if (boostFrames > 0) {
                     // Provide an extra boost for the duration of the extra boost frames
                     this.LiftSpeed.Y = -1 / Engine.DeltaTime;
