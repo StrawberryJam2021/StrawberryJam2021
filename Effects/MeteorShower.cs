861851b86ea5762686f760b7b7a3cfe51f4b5c53
amctyg <amctyg@umsystem.edu>
2022-11-14 00:01:08 -0600

    remove unused imports

--- Effects/MeteorShower.cs
+++ Effects/MeteorShower.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Celeste;
-using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
 using Monocle;
 
--- Triggers/MeteorShowerCountTrigger.cs
+++ Triggers/MeteorShowerCountTrigger.cs
@@ -1,11 +1,6 @@
 ﻿using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
-using Monocle;
 using Celeste.Mod.StrawberryJam2021.Effects;
-using System.Collections.Generic;
-using MonoMod.RuntimeDetour;
-using MonoMod.Utils;
-using System;
 
 namespace Celeste.Mod.StrawberryJam2021.Triggers {
     [CustomEntity("SJ2021/MeteorShowerCountTrigger")]
