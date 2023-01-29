b7a9e033b68b3b6c826c84653983ce260f206063
bigkahuna443 <13278973+bigkahuna443@users.noreply.github.com>
2023-01-29 14:49:00 -0500

    Add setting names
    Allows for translation

--- StrawberryJam2021Settings.cs
+++ StrawberryJam2021Settings.cs
@@ -2,8 +2,10 @@
 
 namespace Celeste.Mod.StrawberryJam2021 {
     public class StrawberryJam2021Settings : EverestModuleSettings {
+        [SettingName("StrawberryJam2021_DisplayDashSequence")]
         public bool DisplayDashSequence { get; set; } = false;
 
+        [SettingName("StrawberryJam2021_TogglePlaybacks")]
         [DefaultButtonBinding(Buttons.Back, Keys.Tab)]
         public ButtonBinding TogglePlaybacks { get; set; }
     }
