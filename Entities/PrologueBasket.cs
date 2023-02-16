11ddcea4dac4705f79bbcdb5e15bfd1d75ba8438
amctyg <amctyg@umsystem.edu>
2023-02-15 20:50:08 -0600

    update logo sprites, move talker position
    i am commiting directly on main >:)

--- Entities/PrologueBasket.cs
+++ Entities/PrologueBasket.cs
@@ -12,7 +12,7 @@ namespace Celeste.Mod.StrawberryJam2021.Entities {
 
         public PrologueBasket(EntityData data, Vector2 offset) : base(data.Position + offset) {
             base.Depth = 2000;
-            Add(talk = new TalkComponent(new Rectangle(-24, -8, 48, 40), new Vector2(0f, 0f), Interact));
+            Add(talk = new TalkComponent(new Rectangle(-24, -8, 48, 40), new Vector2(0f, -8f), Interact));
             talk.PlayerMustBeFacing = false;
             position = data.Position + offset;
             sprite = GFX.Game["objects/StrawberryJam2021/prologueBasket/basket"];
